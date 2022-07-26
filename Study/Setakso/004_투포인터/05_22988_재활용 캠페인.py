bdca2bcd19eba876ca399d1c586f70ace1757586
HyunseokCheong <jhs710000@gmail.com>
2022-07-27 08:40:52 +0900

    0727

--- Study/Setakso/004_투포인터/05_22988_재활용 캠페인.py
+++ Study/Setakso/004_투포인터/05_22988_재활용 캠페인.py
@@ -3,24 +3,24 @@ input = sys.stdin.readline
 
 N, X = map(int, input().split())
 C = list(map(int, input().split()))
+C.sort()  # 정렬
 res = 0  # 결과 값
-# 두개를 합쳤을 때 절반은 넘으면서 최대한 절반에 가깝게 ?
-# 총 용량 이상인거는 아예 빼버리자
-left = 0
-right = N - 1
+res += C.count(X)  # 꽉찬 병 찾아서 리스트에서 빼고 결과 값에 더해줌
+for i in range(res):
+    C.pop()
 
-#
-while left != right:
-    if C[right] >= X:
-        print(left, right, res)
-        res += 1
-        right -= 1
-    elif C[left] + C[right] >= N / 2:
-        print(left, right, res)
-        res += 1
-        left += 1
-        right -= 1
-    elif C[left] + C[right] < N / 2:
-        print(left, right, res)
-        left += 1
-print(res)
+S = 0  # 투포인터 시작
+E = N - 1 - res  # 투포인터 끝 (꽉착 병 뺀거 반영)
+cnt = N - res  # 남은 병
+
+while S < E:
+    if C[S] + C[E] >= X / 2:  # 2병 합친게 절반 이상이면
+        res += 1  # 꽉찬 병 + 1
+        cnt -= 2  # 쓰던 병 - 1
+        S += 1  # 포인터 조절
+        E -= 1
+    else:
+        S += 1  # 포인터 조절
+print(res + cnt // 3)
+# 무조건 3병을 가져가면 꽉찬 병 1병을 만들 수 있다.
+# 이게 포인트였음 ㅠ
--- Study/Setakso/004_투포인터/11_1806_부분합.py
+++ Study/Setakso/004_투포인터/11_1806_부분합.py
@@ -1,21 +1,18 @@
 N, S = map(int, input().split())
 seq = list(map(int, input().split()))
-left = 0
-right = 0
+i, j = 0, 0
 res = seq[0]
 ans = 100001
 
 while True:
     if res >= S:
-        res -= N[left]
-        ans = min(ans, right - left + 1)
-        left += 1
+        res -= seq[i]
+        ans = min(ans, j - i + 1)
+        i += 1
     else:
-        right += 1
-        if right == N:
+        j += 1
+        if j == N:
             break
-        res += seq[right]
-if ans == 100001:
-    print(0)
-else:
-    print(ans)
+        res += seq[j]
+
+print(0) if ans == 100001 else print(ans)
