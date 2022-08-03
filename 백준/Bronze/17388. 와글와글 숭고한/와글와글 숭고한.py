b51e75f1d5b22403537d4593cabb7c53e26c7161
HyunseokCheong <jhs710000@gmail.com>
2022-08-03 23:55:59 +0900

    WTF

--- 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.py
+++
@@ -1,6 +0,0 @@
-SKH = list(map(int, input().split()))
-univ = ['Soongsil', 'Korea', 'Hanyang']
-if sum(SKH) >= 100:
-    print('OK')
-else:
-    print(univ[SKH.index(min(SKH))])
--- 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.py
+++
@@ -1,7 +0,0 @@
-N = int(input())
-for i in range(N):
-    str = input()
-    if 6 <= len(str) <= 9:
-        print('yes')
-    else:
-        print('no')
