c3010235e63da2e4126597ef7e335597978645f2
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-13 18:11:38 +0900

    [D3] Title: 부분 수열의 합, Time: 341 ms, Memory: 169,920 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,3 +1,9 @@
+'''
+현재 요소가 K인가?
+현재 요소와 전 요소 중 하나의 합이 K인가?
+현재 요소와 전 요소 중 여러 개의 합이 K인가?
+합이 K 보다 작은 경우 탐색할 배열에 추가하고 탐색 범위를 늘려주면 OK
+'''
 # input
 for tc in range(int(input())):
     N, K = map(int, input().split())
