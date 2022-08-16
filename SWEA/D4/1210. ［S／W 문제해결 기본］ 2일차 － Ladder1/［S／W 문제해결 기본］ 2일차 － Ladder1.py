5c0fa79cd1fbde26f60e18ef8f15c65653653424
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-16 11:24:48 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 191 ms, Memory: 61,424 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
@@ -1,36 +1,38 @@
-# 진로 설정: 상, 우 좌
 dr = [-1, 0, 0]
 dc = [0, 1, -1]
 
-# input
-for tc in range(10):
-    T = int(input())
-    # 벽 + 사다리 + 벽 생성
-    ladder = [[0] + list(map(int, input().split())) + [0] for _ in range(100)]
-
-    # implement
-    # 초기 위치, 진로 설정
-    column = 0
-    for j in range(0, 102):
-        if ladder[99][j] == 2:
-            column = j
+# 입력
+for _ in range(10):
+    tc = int(input())
+    ladder = [[0] + list(map(int, input().split())) + [0] for i in range(100)]
+    # 초기 위치, 방향 설정
     row = 99
+    column = 0
+    for i in range(0, 102):
+        if ladder[row][i] == 2:
+            column = i
     direction = 0
-
     # 출발
-    while row != 0:
-        if ladder[row][column -1] == 1:
+    while True:
+        # 도착
+        if row == 0:
+            break
+        # 좌로 이동
+        if ladder[row][column - 1] == 1:
             direction = 2
             while True:
                 column += dc[direction]
                 if ladder[row][column - 1] == 0:
                     break
+        # 우로 이동
         elif ladder[row][column + 1] == 1:
             direction = 1
             while True:
                 column += dc[direction]
                 if ladder[row][column + 1] == 0:
                     break
+        # 위로 이동
         direction = 0
         row += dr[direction]
-    print(f'#{tc + 1} {column - 1}')
+        column += dc[direction]
+    print(f'#{tc} {column - 1}')
