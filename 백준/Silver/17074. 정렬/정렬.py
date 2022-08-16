25759f38ad2120aa40326494a432fc901d2144e3
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-17 08:43:59 +0900

    [Silver II] Title: 정렬, Time: 124 ms, Memory: 42312 KB -BaekjoonHub

--- 백준/Silver/17074. 정렬/정렬.py
+++ 백준/Silver/17074. 정렬/정렬.py
@@ -1,41 +1,26 @@
-n = int(input())
-li = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
 
-c = 0
-to_check = -1
-for i in range(n - 1):
-    if li[i] > li[i + 1]:
-        to_check = i
-        c += 1
-    
-if c == 0:
-    print(n)
+N = int(input())
+nums = list(map(int, input().split()))
+
+result = 0
+nums = [- (10 ** 9)] + nums + [10 ** 9]
+
+temp = []
+for i in range(1, N + 1):
+    if nums[i - 1] > nums[i]:
+        result += 1
+        temp.append(i - 1)
+
+if result == 0:
+    print(N)
+elif result >= 2:
+    print(0)
 else:
-    ip = True
-    nli = li.copy()
-    del nli[to_check]
-    if sorted(nli) != nli:
-        if to_check + 1 >= n:
-            ip = False
-        else:
-            nli = li.copy()
-            del nli[to_check + 1]
-            if sorted(nli) != nli:
-                ip = False
-        
-    if not ip:
-        print(0)
-    else:
-        sli = sorted(li)
-
-        diff_s = -1
-        diff_e = 0
-        for i in range(n):
-            if li[i] != sli[i]:
-                if diff_s == -1:
-                    diff_s = i
-                diff_e = i
-        if diff_e - diff_s == 1:
-            print(2)
-        else:
-            print(1)
+    result = 0
+    if nums[temp[0]] <= nums[temp[0] + 2]:
+        result += 1
+    if nums[temp[0] - 1] <= nums[temp[0] + 1]:
+        result += 1
+    print(result)
