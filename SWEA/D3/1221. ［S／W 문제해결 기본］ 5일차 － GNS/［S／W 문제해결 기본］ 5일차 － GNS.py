fe4ce18019082d6e935de2eefe555f7cc3c4fbf9
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-16 10:12:35 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 241 ms, Memory: 63,556 KB -BaekjoonHub

--- SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
@@ -1,11 +1,13 @@
-arr = ["ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"]
-for i in range(int(input())):
+nums = ["ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"]
+
+for _ in range(int(input())):
     tc, N = input().split()
-    nums = list(input().split())
+    N = int(N)
+    arr = list(input().split())
     temp = []
-    for j in arr:
-        for k in range(0, int(N)):
-            if j == nums[k]:
-                temp.append(nums[k])
+    for i in nums:
+        for j in range(0, N):
+            if i == arr[j]:
+                temp.append(i)
     print(tc)
     print(*temp)
