e4437a7b65ac2452f03bf5f1eae9064a5d73e9a2
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-18 01:34:47 +0900

    [Silver V] Title: 종이자르기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -1,20 +1,29 @@
+def bublle_sort(li):
+    N = len(li)
+    for i in range(N - 1, 0, -1):
+        for j in range(0, i):
+            if li[j] > li[j + 1]:
+                li[j], li[j + 1] = li[j + 1], li[j]
+
+
 width, height = map(int, input().split())
-width_list = [0, width]
-height_list = [0, height]
-for _ in range(int(input())):
-    direction, target = map(int, input().split())
-    if direction == 0:
-        height_list.append(target)
-    else:
-        width_list.append(target)
+X_list = [0, width]
+Y_list = [0, height]
+for k in range(int(input())):
+    direction, position = map(int, input().split())
+    if direction == 1:
+        X_list.append(position)
+    elif direction == 0:
+        Y_list.append(position)
 
-width_list.sort()
-height_list.sort()
-
-area = 0
-for i in range(1, len(width_list)):
-    for j in range(1, len(height_list)):
-        W = width_list[i] - width_list[i - 1]
-        H = height_list[j] - height_list[j - 1]
-        area = max(area, W * H)
-print(area)
+bublle_sort(X_list)
+bublle_sort(Y_list)
+max_X = 0
+max_Y = 0
+for i in range(1, len(X_list)):
+    if X_list[i] - X_list[i - 1] > max_X:
+        max_X = X_list[i] - X_list[i - 1]
+for i in range(1, len(Y_list)):
+    if Y_list[i] - Y_list[i - 1] > max_Y:
+        max_Y = Y_list[i] - Y_list[i - 1]
+print(max_X * max_Y)
