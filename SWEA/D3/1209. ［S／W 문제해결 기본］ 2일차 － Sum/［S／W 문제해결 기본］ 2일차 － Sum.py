2b9f112006d0e61f970eb3d60393016f02df521c
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-18 21:08:59 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 221 ms, Memory: 62,472 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -1,40 +1,35 @@
-'''
-구현 계획: 반복문으로 가로, 세로, 대각선1, 대각선2를 구하고 비교
-'''
-# input
 for tc in range(10):
     T = int(input())
     board = [list(map(int, input().split())) for _ in range(100)]
 
-    # implement
-    max_value = 0
+    max_val = 0
 
     # 가로
-    for row in range(0, 100):
+    for i in range(100):
         temp = 0
-        for column in range(0, 100):
-            temp += board[row][column]
-        if temp > max_value:
-            max_value = temp
+        for j in range(100):
+            temp += board[i][j]
+        if temp > max_val:
+            max_val = temp
 
     # 세로
-    for column in range(0, 100):
+    for i in range(100):
         temp = 0
-        for row in range(0, 100):
-            temp += board[row][column]
-        if temp > max_value:
-            max_value = temp
+        for j in range(100):
+            temp += board[j][i]
+        if temp > max_val:
+            max_val = temp
 
     # 대각선
-    for rc in range(100):
-        temp1 = 0
-        temp2 = 0
-        temp1 += board[rc][rc]
-        temp2 += board[rc][99 - rc]
-    if temp1 > max_value:
-        max_value = temp1
-    if temp2 > max_value:
-        max_value = temp2
-    
-    # output
-    print(f'#{tc + 1} {max_value}')
+    temp1 = 0
+    temp2 = 0
+    for i in range(100):
+        temp1 += board[i][i]
+        temp2 += board[i][99 - i]
+
+    if temp1 > max_val:
+        max_val = temp1
+    if temp2 > max_val:
+        max_val = temp2
+
+    print(f'#{tc + 1} {max_val}')
