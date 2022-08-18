c164c15686ea97c3f3e87b9169e36939e5522f93
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-18 14:43:05 +0900

    [D4] Title: 자기 방으로 돌아가기, Time: 190 ms, Memory: 64,104 KB -BaekjoonHub

---
+++ SWEA/D4/4408. 자기 방으로 돌아가기/자기 방으로 돌아가기.py
@@ -0,0 +1,18 @@
+for tc in range(int(input())):
+    N = int(input())
+    way = [0] * 400
+    for i in range(N):
+        X, Y = map(int, input().split())
+        if X <= Y:
+            start = (X - 1) // 2
+            end = (Y - 1) // 2
+        else:
+            start = (Y - 1) // 2
+            end = (X - 1) // 2
+        for j in range(start, end + 1):
+            way[j] += 1
+    max_value = 0
+    for value in way:
+        if value > max_value:
+            max_value = value
+    print(f'#{tc + 1} {max_value}')
