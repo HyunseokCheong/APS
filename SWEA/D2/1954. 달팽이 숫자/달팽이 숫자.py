f146ff036471e22f19ceb09141035f1098f490a5
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-18 20:15:20 +0900

    [D2] Title: 달팽이 숫자, Time: 130 ms, Memory: 57,180 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,28 +1,27 @@
-'''
-2
-3
-4
-구현 계획: 우하좌상
-'''
+# 우 하 좌 상
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
 
 for tc in range(int(input())):
     N = int(input())
-    snail = [[0] * N for _ in range(N)]
-    r, c = 0, 0
+
+    board = [[0] * N for _ in range(N)]
     direction = 0
+    row = 0
+    column = 0
 
     for i in range(1, N ** 2 + 1):
-        snail[r][c] = i
-        r += dr[direction]
-        c += dc[direction]
-        if r < 0 or c < 0 or r >= N or c >= N or snail[r][c] != 0:
-            r -= dr[direction]
-            c -= dc[direction]
+        board[row][column] = i
+        row += dr[direction]
+        column += dc[direction]
+
+        if row < 0 or column < 0 or row >= N or column >= N or board[row][column] != 0:
+            row -= dr[direction]
+            column -= dc[direction]
             direction = (direction + 1) % 4
-            r += dr[direction]
-            c += dc[direction]
+            row += dr[direction]
+            column += dc[direction]
+    
     print(f'#{tc + 1}')
-    for i in snail:
+    for i in board:
         print(*i)
