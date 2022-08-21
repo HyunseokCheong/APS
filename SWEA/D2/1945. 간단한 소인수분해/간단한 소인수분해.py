13632a9b12379c46e8dd76252aaa3bd17f16e43e
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-21 18:32:40 +0900

    [D2] Title: 간단한 소인수분해, Time: 151 ms, Memory: 58,264 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -1,23 +1,18 @@
+# 입력
 for tc in range(int(input())):
     N = int(input())
 
-    A = B = C = D = E = 0
+    # 구현
+    temp = [2, 3, 5, 7, 11]
+    answer = [0] * 5
+    i = 0
+    while i < 5:
+        if N % temp[i] == 0:
+            N //= temp[i]
+            answer[i] += 1
+        else:
+            i += 1
 
-    while N != 1:
-        if N % 2 == 0:
-            N //= 2
-            A += 1
-        if N % 3 == 0:
-            N //= 3
-            B += 1
-        if N % 5 == 0:
-            N //= 5
-            C += 1
-        if N % 7 == 0:
-            N //= 7
-            D += 1
-        if N % 11 == 0:
-            N //= 11
-            E += 1
-    
-    print(f'#{tc + 1} {A} {B} {C} {D} {E}')
+    # 출력
+    print(f'#{tc + 1}', end=' ')
+    print(*answer)
