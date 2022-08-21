3fec1a313dcaf8c4420e8253ec80372ddc54bbfe
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-21 20:37:18 +0900

    [D2] Title: 스도쿠 검증, Time: 149 ms, Memory: 58,268 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,30 +1,27 @@
 for tc in range(int(input())):
-    board = [list(map(int, input().split())) for i in range(9)]
+    board = [list(map(int, input().split())) for _ in range(9)]
 
     answer = 1
 
-    # 가로, 세로
-    for i in range(0, 9):
-        temp_col = set()
-        temp_row = set()
-        temp_3X3 = set()
-        for j in range(0, 9):
-            temp_col.add(board[i][j])
-            temp_row.add(board[j][i])
+    for i in range(9):
+        set_row = set()
+        set_col = set()
+        set_3X3 = set()
 
-            # 3X3
+        for j in range(9):
+            set_col.add(board[i][j])
+            set_row.add(board[j][i])
+
             if i % 3 == 0 and j % 3 == 0:
                 for k in range(i, i + 3):
                     for l in range(j, j + 3):
-                        temp_3X3.add(board[k][l])
+                        set_3X3.add(board[k][l])
 
-                # 3X3 검증
-                if len(temp_3X3) != 9:
+                if len(set_3X3) != 9:
                     answer = 0
                     break
 
-        # 가로, 세로 검증
-        if len(temp_col) != 9 or len(temp_row) != 9:
+        if len(set_row) != 9 or len(set_col) != 9:
             answer = 0
             break
 
