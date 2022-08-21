671d7eeaceff064ddc62eaa90654ddb4bf93c8fe
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-21 19:40:25 +0900

    [D3] Title: 삼성시의 버스 노선, Time: 343 ms, Memory: 64,352 KB -BaekjoonHub

--- SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
+++ SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
@@ -1,16 +1,14 @@
 for tc in range(int(input())):
-    N = int(input())
     course = [0] * 5001
 
-    for i in range(N):
+    for i in range(int(input())):
         A, B = map(int, input().split())
         for j in range(A, B + 1):
             course[j] += 1
 
     print(f'#{tc + 1}', end=' ')
-
-    P = int(input())
-    for i in range(P):
+    
+    for i in range(int(input())):
         C = int(input())
         print(course[C], end=' ')
     print()
