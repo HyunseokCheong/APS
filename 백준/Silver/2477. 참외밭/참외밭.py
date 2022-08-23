6c06758c60619e2c0a16346e361ab14e73cce982
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-23 13:36:55 +0900

    [Silver III] Title: 참외밭, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -1,29 +1,32 @@
 K = int(input())
-max_height = 0  # 가장 긴 높이를 저장할 변수
-max_width = 0  # 가장 긴 가로길이를 저장할 변수
-max_width_index = 0  # 가장 긴 가로길이의 인덱스를 저장할 변수
-max_height_index = 0  # 가장 긴 높이의 인덱스를 저장할 변수
-info = []  # 변의 정보들을 저장할 리스트
+
+D_list = []
+L_list = []
+
 for i in range(6):
-    temp = list(map(int, input().split()))
-    info.append(temp)
-    if temp[0] == 1 or temp[0] == 2:  # 만약 가로 길이라면
-        if max_width < temp[1]:  # 해당 길이가 가장 길다면
-            max_width = temp[1]  # 그 길이를 가장긴 가로로 저장한 후
-            max_width_index = i  # 그에 해당하는 인덱스를 저장
-    else:
-        if max_height < temp[1]:  # 해당 길이가 가장 길다면
-            max_height = temp[1]  # 그 길이를 가장긴 세로로 저장한 후
-            max_height_index = i  # 그에 해당하는 인덱스를 저장
+    D, L = map(int, input().split())
+    D_list.append(D)
+    L_list.append(L)
 
-# 그 후 가장 긴 가로 및 세로와 인접한 변 2개와 가장긴 가로와 세로 총 4개를 새로운 리스트에 저장한다.
-index_list = [info[max_height_index - 1], info[(max_height_index + 1) % 6], info[max_width_index - 1],
-              info[(max_width_index + 1) % 6]]
+max_width = 0
+max_width_idx = 0
+max_height = 0
+max_height_idx = 0
 
-product = 1  # 곱을 저장할 변수
-for i in info:  # 입력받은 변들 중에
-    if i not in index_list:  # 만약 새로운 리스트에 저장한 변 4개 중에 없다면 빈 사각형이므로
-        product *= i[1]  # 그 넓이를 변수에 저장한다.
+for i in range(6):
+    if (D_list[i] == 1 or D_list[i] == 2) and L_list[i] > max_width:
+        max_width = L_list[i]
+        max_width_idx = i
+    elif (D_list[i] == 3 or D_list[i] == 4) and L_list[i] > max_height:
+        max_height = L_list[i]
+        max_height_idx = i
 
-result = (max_width * max_height - product) * K  # 전체 큰 직사각형 넓이에서 빈 사각형 넓이를 빼준 후 K를 곱한다.
-print(result)
+temp_width = L_list[max_height_idx - 1] - L_list[(max_height_idx + 1) % 6]
+if temp_width < 0:
+    temp_width = -temp_width
+
+temp_height = L_list[max_width_idx - 1] - L_list[(max_width_idx + 1) % 6]
+if temp_height < 0:
+    temp_height = -temp_height
+
+print(K * ((max_width * max_height) - (temp_width * temp_height)))
