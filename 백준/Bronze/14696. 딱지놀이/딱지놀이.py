e192606b7bc27832915dd4c454c59981bbce8d22
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-25 17:40:27 +0900

    [Bronze I] Title: 딱지놀이, Time: 208 ms, Memory: 120616 KB -BaekjoonHub

--- 백준/Bronze/14696. 딱지놀이/딱지놀이.py
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.py
@@ -1,35 +1,23 @@
+import sys
+
+input = sys.stdin.readline
 for tc in range(int(input())):
-    A_list = list(map(int, input().split()))
-    B_list = list(map(int, input().split()))
+    A, *A_card = map(int, input().split())
+    B, *B_card = map(int, input().split())
 
-    fight_list = [0] * 4
-    for i in range(1, len(A_list)):
-        if A_list[i] == 4:
-            fight_list[0] += 1
-        elif A_list[i] == 3:
-            fight_list[1] += 1
-        elif A_list[i] == 2:
-            fight_list[2] += 1
-        elif A_list[i] == 1:
-            fight_list[3] += 1
-    for i in range(1, len(B_list)):
-        if B_list[i] == 4:
-            fight_list[0] -= 1
-        elif B_list[i] == 3:
-            fight_list[1] -= 1
-        elif B_list[i] == 2:
-            fight_list[2] -= 1
-        elif B_list[i] == 1:
-            fight_list[3] -= 1
+    fight = [0] * 5
 
-    for i in fight_list:
-        if i > 0:
+    for i in range(A):
+        fight[A_card[i]] += 1
+    for i in range(B):
+        fight[B_card[i]] -= 1
+
+    for i in range(4, 0, -1):
+        if fight[i] > 0:
             print('A')
             break
-        elif i < 0:
+        elif fight[i] < 0:
             print('B')
             break
-        else:
-            continue
     else:
         print('D')
