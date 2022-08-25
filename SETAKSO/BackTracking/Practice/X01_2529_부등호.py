8bea61a147933809d0cc7974c49c4b256fb6569f
hyunseokcheong <jhs710000@gmail.com>
2022-08-26 00:59:22 +0900

    0825

---
+++ SETAKSO/BackTracking/Practice/X01_2529_부등호.py
@@ -0,0 +1,21 @@
+# 한 케이스 내에 같은 수가 없는 것들만 출력
+def recur2(cur):
+    # 여기에 if문 넣어서 걸러주자
+
+    if cur == N:
+        print(*arr)
+        return
+    for i in range(M):
+        if visited[i]:
+            continue
+        arr[cur] = i
+        visited[i] = True
+        recur2(cur + 1)
+        visited[i] = False
+
+
+N, M = map(int, input().split())
+arr = [0 for i in range(N)]
+visited = [False for i in range(M)]
+
+recur2(0)
