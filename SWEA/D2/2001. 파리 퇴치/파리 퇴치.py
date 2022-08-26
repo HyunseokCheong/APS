59a8cac39286124bc7823d8f55bdbd0fd3576eee
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-26 09:38:35 +0900

    [D2] Title: 파리 퇴치, Time: 163 ms, Memory: 61,200 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,20 +1,14 @@
-# 입력
 for tc in range(int(input())):
     N, M = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(N)]
 
-    # 구현
-    answer = 0
-    for i in range(0, N - M + 1):
-        for j in range(0, N - M + 1):
-
+    max_val = 0
+    for row in range(0, N - M + 1):
+        for col in range(0, N - M + 1):
             temp = 0
-            for k in range(i, i + M):
-                for l in range(j, j + M):
-                    temp += board[k][l]
-
-            if temp > answer:
-                answer = temp
-
-    # 출력
-    print(f'#{tc + 1} {answer}')
+            for i in range(row, row + M):
+                for j in range(col, col + M):
+                    temp += board[i][j]
+            if temp > max_val:
+                max_val = temp
+    print(f'#{tc + 1} {max_val}')
