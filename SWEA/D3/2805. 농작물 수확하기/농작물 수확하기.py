7849f98111c7d3a03d6947e7f308471684c3d564
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-26 13:40:24 +0900

    [D3] Title: 농작물 수확하기, Time: 201 ms, Memory: 63,032 KB -BaekjoonHub

---
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -0,0 +1,13 @@
+for tc in range(int(input())):
+    N = int(input())
+
+    answer = 0
+    for i in range(N):
+        word = input()
+        if i > N // 2:
+            i = i - (2 * (i - N // 2))
+        if i <= N // 2:
+            for j in range(N // 2 - i, N // 2 + i + 1):
+                answer += int(word[j])
+    
+    print(f'#{tc + 1} {answer}')
