dab64e0745d53c1fa8a70f01c63ae7ae8769979b
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-28 20:18:22 +0900

    [D2] Title: 파리 퇴치, Time: 163 ms, Memory: 61,200 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -5,10 +5,11 @@ for tc in range(int(input())):
     max_val = 0
     for row in range(0, N - M + 1):
         for col in range(0, N - M + 1):
-            temp = 0
+            res = 0
             for i in range(row, row + M):
-                for j in range(col, col + M):
-                    temp += board[i][j]
-            if temp > max_val:
-                max_val = temp
+                for k in range(col, col + M):
+                    res += board[i][k]
+            if res > max_val:
+                max_val = res
+
     print(f'#{tc + 1} {max_val}')
