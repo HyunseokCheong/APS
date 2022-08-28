3a2afc16ae1976d0b7b88f41c74f7bb5c10b7305
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-08-28 22:09:51 +0900

    [D3] Title: 현주의 상자 바꾸기, Time: 561 ms, Memory: 65,144 KB -BaekjoonHub

--- SWEA/D3/5789. 현주의 상자 바꾸기/현주의 상자 바꾸기.py
+++ SWEA/D3/5789. 현주의 상자 바꾸기/현주의 상자 바꾸기.py
@@ -1,11 +1,12 @@
 for tc in range(int(input())):
     N, Q = map(int, input().split())
 
-    boxes = [0] * N
+    mat = [0] * N
+
     for i in range(Q):
         L, R = map(int, input().split())
-        for j in range(L-1, R):
-            boxes[j] = i+1
 
-    print(f'#{tc + 1}', end=' ')
-    print(*boxes)
+        for j in range(L - 1, R):
+            mat[j] = i + 1
+
+    print(f'#{tc + 1}', *mat)
