5fe860b37cfdeebf7bf40a05c9483389d78dcf2f
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-09-08 10:16:11 +0900

    [Bronze III] Title: 완전 제곱수, Time: 128 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/6131. 완전 제곱수/완전 제곱수.py
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.py
@@ -1,7 +1,12 @@
+'''
+1 <= B, A <= 500
+1 <= N <= 1000
+'''
 N = int(input())
+
 cnt = 0
-for i in range(1, 501):
-    for j in range(1, 501):
-        if j ** 2 + N == i ** 2:
+for B in range(1, 501):
+    for A in range(B, 501):
+        if B ** 2 + N == A ** 2:
             cnt += 1
 print(cnt)
