8e8eabe75f57dcda6fb5b47b1982021e99dcdae9
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-09-19 19:18:39 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 140 ms, Memory: 57,184 KB -BaekjoonHub

---
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
@@ -0,0 +1,43 @@
+'''
+1 <= N <= 50
+56 <= M <= 100
+'''
+pwd = {
+    '0001101': 0, '0011001': 1, '0010011': 2, '0111101': 3, '0100011': 4,
+    '0110001': 5, '0101111': 6, '0111011': 7, '0110111': 8, '0001011': 9
+}
+
+for tc in range(int(input())):
+    N, M = map(int, input().split())
+
+    # 암호 행 찾기
+    nums = 0
+    for i in range(N):
+        temp = input()
+        if int(temp) != 0:
+            nums = temp
+
+    # 암호가 없는 경우
+    if nums == 0:
+        print(f'#{tc + 1} {0}')
+        continue
+
+    # 암호 열 찾기
+    end_idx = 0
+    for idx in range(len(nums) - 1, -1, -1):
+        if nums[idx] == '1':
+            end_idx = idx
+            break
+    start_idx = end_idx - 56 + 1
+
+    # 암호 변환
+    codes = []
+    for i in range(start_idx, end_idx, 7):
+        codes.append(pwd[nums[i:i + 7]])
+    odd = codes[0] + codes[2] + codes[4] + codes[6]
+    even = codes[1] + codes[3] + codes[5] + codes[7]
+
+    if (odd * 3 + even) % 10 != 0:
+        print(f'#{tc + 1} {0}')
+    else:
+        print(f'#{tc + 1} {odd + even}')
