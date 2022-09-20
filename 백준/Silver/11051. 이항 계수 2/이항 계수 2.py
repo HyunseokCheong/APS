20679b997ef7b95282fd2a37e2577707dd4d30e0
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-09-20 21:15:45 +0900

    [Silver III] Title: 이항 계수 2, Time: 108 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,12 @@
+def facto(num):
+    res = 1
+    for i in range(num, 0, -1):
+        res *= i
+    return res
+
+
+n, k = map(int, input().split())
+
+ans = (facto(n) // (facto(n - k) * facto(k))) % 10007
+
+print(ans)
