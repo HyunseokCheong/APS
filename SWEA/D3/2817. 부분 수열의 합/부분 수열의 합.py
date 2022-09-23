487d2b6f447e81aaaabc3f4c48e6eb1fcf719f56
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-09-23 20:44:14 +0900

    [D3] Title: 부분 수열의 합, Time: 341 ms, Memory: 169,920 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,20 +1,19 @@
-# input
-for tc in range(int(input())):
-    N, K = map(int, input().split())
-    nums = list(map(int, input().split()))
-
-    # implement
+t = int(input())
+for tc in range(t):
+    n, k = map(int, input().split())
+    arr = list(map(int, input().split()))
     cnt = 0
     temp = []
-    for i in range(0, N):
-        temp.append(nums[i])
-        if nums[i] == K:
-            cnt += 1
-        for j in range(len(temp) - 1):
-            if nums[i] + temp[j] < K:
-                temp.append(nums[i] + temp[j])
-            elif nums[i] + temp[j] == K:
-                cnt += 1
 
-    # output
+    for i in range(n):
+        if arr[i] == k:
+            cnt += 1
+        else:
+            temp.append(arr[i])
+            for j in range(len(temp) - 1):
+                res = temp[j] + arr[i]
+                if res == k:
+                    cnt += 1
+                elif res < k:
+                    temp.append(res)
     print(f'#{tc + 1} {cnt}')
