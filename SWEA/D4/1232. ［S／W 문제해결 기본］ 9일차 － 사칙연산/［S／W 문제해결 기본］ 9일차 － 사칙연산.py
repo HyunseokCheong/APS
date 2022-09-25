426e8465a44126cafc20323793047ca0f4263c7a
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-09-25 20:50:41 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산, Time: 194 ms, Memory: 62,708 KB -BaekjoonHub

--- SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.py
+++ SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.py
@@ -1,25 +1,23 @@
-def calc(node):
-    if len(tree[node]) == 2:
-        return int(tree[node][1])
-    else:
-        left = calc(int(tree[node][2]))
-        right = calc(int(tree[node][3]))
-        operator = tree[node][1]
-        if operator == '+':
-            return left + right
-        elif operator == '-':
-            return left - right
-        elif operator == '*':
-            return left * right
-        elif operator == '/':
-            return left / right
+def solve(x):
+    if len(tree[x]) == 2:
+        return int(tree[x][1])
+    left = solve(int(tree[x][2]))
+    right = solve(int(tree[x][3]))
+    op = tree[x][1]
+    if op == '+':
+        return left + right
+    elif op == '-':
+        return left - right
+    elif op == '*':
+        return left * right
+    elif op == '/':
+        return left / right
 
 
 for tc in range(10):
-    N = int(input())
-    tree = [0] * (N + 1)
-    for i in range(N):
-        temp = input().split()
-        tree[int(temp[0])] = temp
-    ans = int(calc(1))
-    print(f'#{tc + 1} {ans}')
+    n = int(input())
+    tree = [0] * (n + 1)
+    for i in range(n):
+        arr = input().split()
+        tree[int(arr[0])] = arr
+    print(f'#{tc + 1} {int(solve(1))}')
