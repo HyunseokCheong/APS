3002e5ec3118f4831e7947846298e317771848fe
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-09-27 15:06:42 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 140 ms, Memory: 57,184 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
@@ -1,44 +1,34 @@
-'''
-1 <= N <= 50
-56 <= M <= 100
-'''
 pwd = {
     '0001101': 0, '0011001': 1, '0010011': 2, '0111101': 3, '0100011': 4,
     '0110001': 5, '0101111': 6, '0111011': 7, '0110111': 8, '0001011': 9
 }
 ans = []
 
-for tc in range(int(input())):
-    N, M = map(int, input().split())
-
-    # 암호 행 찾기
+t = int(input())
+for tc in range(t):
+    n, m = map(int, input().split())
     nums = 0
-    for i in range(N):
+    for i in range(n):
         temp = input()
         if nums != 0:
             continue
         elif int(temp) != 0:
             nums = temp
 
-    # 암호 열 찾기
     end_idx = 0
     for idx in range(len(nums) - 1, -1, -1):
         if nums[idx] == '1':
             end_idx = idx
             break
-    start_idx = end_idx - 56 + 1
+    start_idx = end_idx - 55
 
-    # 암호 변환
     codes = []
     for i in range(start_idx, end_idx, 7):
         codes.append(pwd[nums[i:i + 7]])
     odd = codes[0] + codes[2] + codes[4] + codes[6]
     even = codes[1] + codes[3] + codes[5] + codes[7]
 
-    if (odd * 3 + even) % 10 != 0:
-        ans.append([tc + 1, 0])
+    if (odd * 3 + even) % 10 == 0:
+        print(f'#{tc + 1} {odd + even}')
     else:
-        ans.append([tc + 1, odd + even])
-
-for i in ans:
-    print(f'#{i[0]} {i[1]}')
+        print(f'#{tc + 1} 0')
