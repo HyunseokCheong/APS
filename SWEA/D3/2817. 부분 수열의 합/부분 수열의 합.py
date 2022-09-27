56624fb4c2ed22b126c6f7335ba92b6bd33c3573
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-09-27 15:34:33 +0900

    [D3] Title: 부분 수열의 합, Time: 301 ms, Memory: 170,240 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,19 +1,17 @@
+def dfs(depth, value):
+    global cnt
+    if depth >= n:
+        return
+    if value + arr[depth] == k:
+        cnt += 1
+    dfs(depth + 1, value)
+    dfs(depth + 1, value + arr[depth])
+
+
 t = int(input())
 for tc in range(t):
     n, k = map(int, input().split())
     arr = list(map(int, input().split()))
     cnt = 0
-    temp = []
-
-    for i in range(n):
-        if arr[i] == k:
-            cnt += 1
-        else:
-            temp.append(arr[i])
-            for j in range(len(temp) - 1):
-                res = temp[j] + arr[i]
-                if res == k:
-                    cnt += 1
-                elif res < k:
-                    temp.append(res)
+    dfs(0, 0)
     print(f'#{tc + 1} {cnt}')
