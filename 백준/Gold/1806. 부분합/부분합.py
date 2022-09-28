1aaff9499cd37b321eae0e9e106387ecb7f5ba91
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-09-28 22:30:46 +0900

    [Gold IV] Title: 부분합, Time: 144 ms, Memory: 128152 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,19 +1,23 @@
+import sys
+
+input = sys.stdin.readline
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 
-l, r = 0, 1
+st, en = 0, 1
 val = arr[0]
 ans = 0
-while l < n and r <= n:
+
+while st < n and en <= n:
     if val < s:
-        if r < n:
-            val += arr[r]
-            r += 1
+        if en < n:
+            val += arr[en]
+            en += 1
         else:
             break
     elif val >= s:
-        if r - l < ans or ans == 0:
-            ans = r - l
-        val -= arr[l]
-        l += 1
+        if en - st < ans or ans == 0:
+            ans = en - st
+        val -= arr[st]
+        st += 1
 print(ans)
