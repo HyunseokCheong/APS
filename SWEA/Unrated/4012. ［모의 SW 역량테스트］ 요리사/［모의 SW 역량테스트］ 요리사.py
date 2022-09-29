297f764217e1bd9d23e7acc9f893906480d3a154
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-09-30 08:37:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 1,109 ms, Memory: 72,268 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
@@ -1,18 +1,18 @@
-def dfs(idx, depth):
+def dfs(depth, idx):
     global mnv
     if depth == n // 2:
         s1, s2 = 0, 0
         for i in range(n - 1):
             for j in range(i + 1, n):
-                if visited[i] and visited[j]:
+                if visited[i] == 1 and visited[j] == 1:
                     s1 += arr[i][j] + arr[j][i]
-                elif not visited[i] and not visited[j]:
+                elif visited[i] == 0 and visited[j] == 0:
                     s2 += arr[i][j] + arr[j][i]
         mnv = min(mnv, abs(s1 - s2))
     for i in range(idx, n):
-        if not visited[i]:
+        if visited[i] == 0:
             visited[i] = 1
-            dfs(i + 1, depth + 1)
+            dfs(depth + 1, i + 1)
             visited[i] = 0
 
 
