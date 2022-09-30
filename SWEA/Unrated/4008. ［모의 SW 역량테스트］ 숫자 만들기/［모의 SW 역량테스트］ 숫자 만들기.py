c3ec83ce2f8e3232cd98b09f5fadc4016c56dcc7
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-09-30 10:37:28 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 319 ms, Memory: 64,104 KB -BaekjoonHub

---
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.py
@@ -0,0 +1,26 @@
+def dfs(depth, value):
+    global mnv, mxv
+    if depth == n:
+        mnv = min(mnv, value)
+        mxv = max(mxv, value)
+        return
+    calc = [value + num[depth], value - num[depth], value * num[depth], int(value / num[depth])]
+    for i in range(4):
+        if opr[i] > 0:
+            opr[i] -= 1
+            dfs(depth + 1, calc[i])
+            opr[i] += 1
+
+
+t = int(input())
+for tc in range(t):
+    n = int(input())
+    opr = list(map(int, input().split()))
+    num = list(map(int, input().split()))
+
+    mnv = 1e9
+    mxv = -1e9
+    visited = [0] * n
+
+    dfs(1, num[0])
+    print(f'#{tc + 1} {mxv - mnv}')
