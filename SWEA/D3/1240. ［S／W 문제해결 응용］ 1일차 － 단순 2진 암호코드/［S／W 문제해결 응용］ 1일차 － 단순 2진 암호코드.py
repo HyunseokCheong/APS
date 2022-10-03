a5e9b31642ebb76e28706e6a621745f213582f10
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-10-03 11:58:23 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 140 ms, Memory: 57,184 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
@@ -2,31 +2,28 @@ pwd = {
     '0001101': 0, '0011001': 1, '0010011': 2, '0111101': 3, '0100011': 4,
     '0110001': 5, '0101111': 6, '0111011': 7, '0110111': 8, '0001011': 9
 }
-ans = []
-
 t = int(input())
 for tc in range(t):
     n, m = map(int, input().split())
-    nums = 0
+
     for i in range(n):
         temp = input()
-        if nums != 0:
-            continue
-        elif int(temp) != 0:
+        if int(temp) != 0:
             nums = temp
 
-    end_idx = 0
+    eni = 0
     for idx in range(len(nums) - 1, -1, -1):
         if nums[idx] == '1':
-            end_idx = idx
+            eni = idx
             break
-    start_idx = end_idx - 55
+    sti = eni - 55
 
-    codes = []
-    for i in range(start_idx, end_idx, 7):
-        codes.append(pwd[nums[i:i + 7]])
-    odd = codes[0] + codes[2] + codes[4] + codes[6]
-    even = codes[1] + codes[3] + codes[5] + codes[7]
+    code = [0] * 8
+    for i in range(sti, eni + 1, 7):
+        code[(i - sti) // 7] = pwd[nums[i:i + 7]]
+
+    odd = code[0] + code[2] + code[4] + code[6]
+    even = code[1] + code[3] + code[5] + code[7]
 
     if (odd * 3 + even) % 10 == 0:
         print(f'#{tc + 1} {odd + even}')
