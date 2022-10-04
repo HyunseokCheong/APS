cea11a933cc08f92a41edcea684e123272de7158
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-10-05 01:00:20 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 115632 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,11 +1,14 @@
 k, n = map(int, input().split())
-arr = [int(input()) for _ in range(k)]
+arr = [0] * k
+for i in range(k):
+    arr[i] = int(input())
+
 s, e = 1, max(arr)
 while s <= e:
     m = (s + e) // 2
     cnt = 0
-    for i in arr:
-        cnt += i // m
+    for i in range(k):
+        cnt += arr[i] // m
     if cnt >= n:
         s = m + 1
     else:
