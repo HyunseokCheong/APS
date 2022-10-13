ecf163d96f4c4b58f6d921bde1d923077c589878
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-10-13 20:59:12 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -3,17 +3,17 @@ import sys
 input = sys.stdin.readline
 
 
-def clean(r, c, d):
+def solve(r, c, d):
     global ans
     if arr[r][c] == 0:
         arr[r][c] = 2
         ans += 1
-    for _ in range(4):
+    for i in range(4):
         nd = (d + 3) % 4
         nr = r + dr[nd]
         nc = c + dc[nd]
         if arr[nr][nc] == 0:
-            clean(nr, nc, nd)
+            solve(nr, nc, nd)
             return
         d = nd
     nd = (d + 2) % 4
@@ -21,17 +21,16 @@ def clean(r, c, d):
     nc = c + dc[nd]
     if arr[nr][nc] == 1:
         return
-    clean(nr, nc, d)
+    solve(nr, nc, d)
 
-
-# 북 동 남 서
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
 
 n, m = map(int, input().split())
 r, c, d = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
+
 ans = 0
-clean(r, c, d)
+solve(r, c, d)
 print(ans)
