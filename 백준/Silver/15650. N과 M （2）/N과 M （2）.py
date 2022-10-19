ceff95cd482fd30a48346edd741f1cb766d21082
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-10-19 21:44:38 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,13 @@
+def solve(depth, start):
+    if depth == n:
+        print(*arr)
+        return
+    for i in range(start, m):
+        arr[depth] = i + 1
+        solve(depth + 1, i + 1)
+
+
+m, n = map(int, input().split())
+arr = [0] * n
+
+solve(0, 0)
