83ea9b8c8e4142955a56a1c8643ef2f9df078ab6
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-11-29 10:36:39 +0900

    [Silver IV] Title: 제로, Time: 188 ms, Memory: 21204 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,23 +1,22 @@
 import java.io.*;
 import java.util.Stack;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Stack<Integer> stack = new Stack<>();
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(br.readLine());
-            if (num == 0) stack.pop();
-            else stack.push(num);
-        }
+        int k = Integer.parseInt(br.readLine());
         int ans = 0;
-        while (!stack.empty()) ans += stack.pop();
-        bw.write(ans + "\n");
-        bw.flush();
-        bw.close();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < k; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num != 0) {
+                ans += num;
+                stack.push(num);
+            } else {
+                ans -= stack.pop();
+            }
+        }
+        System.out.println(ans);
     }
 }
