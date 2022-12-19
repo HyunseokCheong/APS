b5254d59211c39ca2e27b9428e5ffdc67cc3a530
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-12-20 03:38:08 +0900

    Delete SWEA directory

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
+++
@@ -1,7 +0,0 @@
-t = int(input())
-for tc in range(t):
-    s = input()
-    ans = 0
-    if s == s[::-1]:
-        ans = 1
-    print(f'#{tc + 1} {ans}')
--- SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-for tc in range(t):
-    s = [list(input()) for _ in range(5)]
-    ans = ''
-    for i in range(15):
-        for j in range(5):
-            if len(s[j]) > i:
-                ans += s[j][i]
-    print(f'#{tc + 1} {ans}')
