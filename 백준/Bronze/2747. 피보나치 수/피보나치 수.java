8247bb5f83c4de1d3a97294072ef8a0494289df7
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-12-27 20:52:22 +0900

    [Bronze II] Title: 피보나치 수, Time: 132 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    // declare
+    static int n;
+    static int answer;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        // read
+        n = Integer.parseInt(br.readLine());
+
+        // solve
+        dp = new int[n + 1];
+        answer = fibonacci(n);
+
+        // print
+        bw.write(answer + "");
+        bw.flush();
+        bw.close();
+    }
+
+    static int fibonacci(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        dp[1] = dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
