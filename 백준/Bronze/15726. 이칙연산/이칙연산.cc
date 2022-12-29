158c728d06e62de451f0929668751092ce54e2c3
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2022-12-29 18:47:55 +0900

    [Bronze IV] Title: 이칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15726. 이칙연산/이칙연산.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long a, b, c, x, y;
+    cin >> a >> b >> c;
+    x = (double)(a * b) / c;
+    y = ((double) a / b) * c;
+    
+    if (x > y) {
+        cout << x;
+    } else {
+        cout << y;
+    }
+    return 0;
+}
