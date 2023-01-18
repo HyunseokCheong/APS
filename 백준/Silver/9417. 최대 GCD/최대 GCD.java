8e690d3c08c6f991d8484638e6155dbd6169d353
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-01-18 09:13:24 +0900

    [Silver IV] Title: 최대 GCD, Time: 136 ms, Memory: 14584 KB -BaekjoonHub

--- 백준/Silver/9417. 최대 GCD/최대 GCD.java
+++ 백준/Silver/9417. 최대 GCD/최대 GCD.java
@@ -1,14 +1,3 @@
-/**
- * N 테케 (1 < N < 100)
- * M 정수의 개수 (1 < M < 100)
- * num 정수 (-2^31 < num < 2^31 -1) int 쓰믄댐
- * 1. 모든 두 수의 쌍을 만든다.
- * 2. 만들면서 두 수의 최대공약수를 구하고 최대 값인지 갱신해줌
- *
- * @Author HyunseokCheong
- * @Date 2023-01-16
- */
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -26,39 +15,42 @@ public class Main {
     static int N;
     static ArrayList<Integer> arrayList;
     static int arrayListSize;
-    static int curGCD, maxVal;
+    static int curGcd, maxGcd;
     
     public static void main(String[] args) throws IOException {
+        // read
         N = Integer.parseInt(br.readLine());
         while (N-- > 0) {
             arrayList = new ArrayList<>();
-            
             st = new StringTokenizer(br.readLine());
             while (st.hasMoreTokens()) {
                 arrayList.add(Integer.parseInt(st.nextToken()));
             }
             
-            maxVal = Integer.MIN_VALUE;
+            // solve
+            maxGcd = Integer.MIN_VALUE;
             arrayListSize = arrayList.size();
-            for (int i = 0; i < arrayListSize - 1; i++) {
-                for (int j = i + 1; j < arrayListSize; j++) {
-                    if (i == j) {
+            for (int j = 0; j < arrayListSize - 1; j++) {
+                for (int k = j + 1; k < arrayListSize; k++) {
+                    if (j == k) {
                         continue;
                     }
-                    curGCD = gcd(arrayList.get(i), arrayList.get(j));
-                    maxVal = Math.max(maxVal, curGCD);
+                    curGcd = GCD(arrayList.get(j), arrayList.get(k));
+                    maxGcd = Math.max(maxGcd, curGcd);
                 }
             }
-            bw.write(maxVal + "\n");
+            bw.write(maxGcd + "\n");
         }
+        
+        // write
         bw.flush();
         bw.close();
     }
     
-    static int gcd(int a, int b) {
+    static int GCD(int a, int b) {
         if (b == 0) {
             return a;
         }
-        return gcd(b, a % b);
+        return GCD(b, a % b);
     }
 }
