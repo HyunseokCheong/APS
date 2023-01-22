e7630679ddcb6859b646c9e1475bf29d1b0b6d8b
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-01-22 23:21:08 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {
-    int dwf[10];
-    int sum_val = 0;
-    for (int i = 0; i < 9; ++i) {
-        cin >> dwf[i];
-        sum_val += dwf[i];
+    int arr[9];
+    int sum = 0;
+    for (int i = 0; i < 9; i++) {
+        cin >> arr[i];
+        sum += arr[i];
     }
-    sort(dwf, dwf + 9);
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 9; ++j) {
-            if (sum_val - (dwf[i] + dwf[j]) == 100) {
-                for (int k = 0; k < 9; ++k) {
+    
+    sort(arr, arr + 9);
+    for (int i = 0; i < 8; i++) {
+        for (int j =0; j < 9; j++) {
+            if (sum - (arr[i] + arr[j]) == 100) {
+                for (int k = 0; k < 9; k++) {
                     if (k != i && k != j) {
-                        cout << dwf[k] << '\n';
+                        cout << arr[k] << endl;
                     }
                 }
                 return 0;
