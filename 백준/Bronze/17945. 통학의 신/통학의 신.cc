b1244a46e8403657b3b09d3460b09cdb0030fd6b
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-01-22 18:42:42 +0900

    [Bronze III] Title: 통학의 신, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.cc
+++ 백준/Bronze/17945. 통학의 신/통학의 신.cc
@@ -1,18 +1,14 @@
-#include <iostream>
-#include <cmath>
-
+#include <bits/stdc++.h>
 using namespace std;
+
 int main() {
-     int A, B;
-
-     cin>>A>>B;
-
-     for (int i = -1000; i < 1000; i++)
-     {
-          if (pow(i, 2) + (2 * A * i) + B == 0)
-          {
-               cout<<i<<"\n";
-          }
-     }
-     
+    int A, B;
+    cin >> A >> B;
+    
+    for (int x = -1000; x < 1001; x++) {
+        if((x * x) + (2 * A * x) + B == 0) {
+            cout << x << " ";
+        }
+    }
+    return 0;
 }
