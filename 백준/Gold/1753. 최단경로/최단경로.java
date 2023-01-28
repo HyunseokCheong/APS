dae992c8d8db83746df9e43fd471623f189afef9
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-01-29 00:54:04 +0900

    [Gold IV] Title: 최단경로, Time: 796 ms, Memory: 108320 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,16 +1,3 @@
-/**
- * 다익스트라 : 한 노드에서 모든 노드로 가는 최단거리를 구한다.
- * 최단거리를 저장하는 배열 + 우선순위 큐를 이용하여 구현한다.
- * 정점의 개수 V (1 <= V <= 20,000)
- * 간선의 개수 E (1 <= E <= 300,000)
- * 모든 정점에는 1부터 V까지 번호가 매겨져 있다.
- * 시작 정점 번호 K (1 <= K <= V)
- * u에서 v로 가는 가중치 w (u, v, w)
- *
- * @Author HyunseokCheong
- * @Date 2023/01/28
- */
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -21,40 +8,43 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-
 public class Main {
     
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
     // declare
-    static int V, E, K;
+    static int V, E;
+    static int K;
     static int u, v, w;
     static ArrayList<ArrayList<Node>> graph;
     static int[] dist;
     static PriorityQueue<Node> queue;
+    static Node curNode, nextNode;
     
     static class Node {
         
-        int idx, weight;
+        int index;
+        int weight;
         
-        public Node(int idx, int weight) {
-            this.idx = idx;
+        public Node(int index, int weight) {
+            this.index = index;
             this.weight = weight;
         }
     }
     
     public static void main(String[] args) throws IOException {
+        // read
         st = new StringTokenizer(br.readLine());
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(br.readLine());
         
-        // 2차원 그래프
         graph = new ArrayList<ArrayList<Node>>();
         for (int i = 0; i < V + 1; i++) {
             graph.add(new ArrayList<Node>());
         }
+        
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             u = Integer.parseInt(st.nextToken());
@@ -63,33 +53,29 @@ public class Main {
             graph.get(u).add(new Node(v, w));
         }
         
-        // dist 배열을 MAX로 초기화
+        // solve
         dist = new int[V + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         
-        // 우선순위 큐 선언
-        // K와 K -> K의 가중치 0 입력
-        // K까지 최단경로 0
-        queue = new PriorityQueue<Node>((o1, o2) -> Integer.compare(o1.weight, o2.weight));
-        queue.offer(new Node(K, 0));
+        queue = new PriorityQueue<Node>(((o1, o2) -> Integer.compare(o1.weight, o2.weight)));
+        queue.add(new Node(K, 0));
         dist[K] = 0;
-        
-        // 탐색 시작
+    
         while (!queue.isEmpty()) {
-            Node curNode = queue.poll();
-            if (dist[curNode.idx] < curNode.weight) {
+            curNode = queue.poll();
+            if (dist[curNode.index] < curNode.weight) {
                 continue;
             }
-            
-            for (int i = 0; i < graph.get(curNode.idx).size(); i++) {
-                Node nextNode = graph.get(curNode.idx).get(i);
-                if (dist[nextNode.idx] > curNode.weight + nextNode.weight) {
-                    dist[nextNode.idx] = curNode.weight + nextNode.weight;
-                    queue.add(new Node(nextNode.idx, dist[nextNode.idx]));
+            for (int i = 0; i < graph.get(curNode.index).size(); i++) {
+                nextNode = graph.get(curNode.index).get(i);
+                if (dist[nextNode.index] > curNode.weight + nextNode.weight) {
+                    dist[nextNode.index] = curNode.weight + nextNode.weight;
+                    queue.add(new Node(nextNode.index, dist[nextNode.index]));
                 }
             }
         }
         
+        // write
         for (int i = 1; i < V + 1; i++) {
             if (dist[i] == Integer.MAX_VALUE) {
                 bw.write("INF" + "\n");
