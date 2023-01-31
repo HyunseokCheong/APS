6df80ec49467202cc3ebd2964d759d426b24b02d
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-02-01 03:07:02 +0900

    [Silver III] Title: 수열, Time: 296 ms, Memory: 23544 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,22 +1,51 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int[] arr = new int[n];
-        int mxv = Integer.MIN_VALUE;
-        
-        for (int i = 0; i < n; i++)
-            arr[i] = sc.nextInt();
-        for (int i = 0; i <= n - k; i++) {
-            int res = 0;
-            for (int j = i; j < i + k; j++) {
-                res += arr[j];
-            }
-            mxv = Math.max(mxv, res);
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    // declare
+    static int N, K;
+    static int[] arr;
+    static int curVal, maxVal, temp;
+    
+    public static void main(String[] args) throws IOException {
+        // read
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(mxv);
+        
+        // solve
+        curVal = 0;
+        for (int i = 0; i < K; i++) {
+            curVal += arr[i];
+        }
+        maxVal = curVal;
+        
+        temp = K;
+        while (true) {
+            if (temp >= N) {
+                break;
+            }
+            curVal += arr[temp] - arr[temp - K];
+            maxVal = Math.max(maxVal, curVal);
+            temp++;
+        }
+        
+        // write
+        bw.write(maxVal + "");
+        bw.flush();
+        bw.close();
     }
 }
