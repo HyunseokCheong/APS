276b57bcd2fa8c6876d0b29cc5fce3e71049e2a9
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-02-11 23:33:55 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 141 ms, Memory: 31,616 KB -BaekjoonHub

--- SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -3,83 +3,167 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Solution {
     
     // declare
-    static BufferedReader     br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter     bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer    st;
-    static ArrayList<Integer> arrayList;
-    static int                N, Q;
+    static BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter  bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    
+    static int passwordN, commandN;
     static char command;
-    static int  commandIndex, commandNumber, commandCount;
+    static int  commandIndex, commandCount, commandNumber;
+    static int[] passwordArr, commandNumberArr;
+    
+    static class Node {
+        
+        int  data;
+        Node next;
+        
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    
+    static class LinkedList {
+        
+        Node   head;
+        Node   tail;
+        Node[] nodeArr;
+        int    nodeCount;
+        
+        public LinkedList() {
+            this.head      = null;
+            this.nodeArr   = new Node[5000];
+            this.nodeCount = 0;
+        }
+        
+        Node getNewNode(int data) {
+            nodeArr[nodeCount] = new Node(data);
+            return nodeArr[nodeCount++];
+        }
+        
+        void insert(int commandIndex, int[] commandNumberArr) {
+            int start = 0;
+            if (commandIndex == 0) {
+                if (head != null) {
+                    Node newNode = getNewNode(commandNumberArr[0]);
+                    newNode.next = head;
+                    head         = newNode;
+                } else {
+                    head = getNewNode(commandNumberArr[0]);
+                }
+                
+                commandIndex = 1;
+                start        = 1;
+            }
+            
+            Node currentNode = head;
+            for (int i = 1; i < commandIndex; i++) {
+                currentNode = currentNode.next;
+            }
+            
+            for (int i = start; i < commandNumberArr.length; i++) {
+                Node newNode = getNewNode(commandNumberArr[i]);
+                newNode.next     = currentNode.next;
+                currentNode.next = newNode;
+                currentNode      = newNode;
+            }
+            if (currentNode.next == null) {
+                tail = currentNode;
+            }
+        }
+        
+        void delete(int commandIndex, int commandCount) {
+            Node currentNode = head;
+            if (commandIndex == 0) {
+                for (int i = 0; i < commandCount; i++) {
+                    currentNode = currentNode.next;
+                }
+                head = currentNode;
+            }
+            
+            for (int i = 1; i < commandIndex; i++) {
+                currentNode = currentNode.next;
+            }
+            
+            Node temp = currentNode;
+            for (int i = 0; i < commandCount; i++) {
+                currentNode = currentNode.next;
+            }
+            temp.next = currentNode.next;
+            
+            if (temp.next == null) {
+                tail = temp;
+            }
+        }
+        
+        void add(int commandNumber) {
+            Node currentNode = tail;
+            Node newNode     = getNewNode(commandNumber);
+            currentNode.next = newNode;
+            tail             = newNode;
+        }
+        
+        void print() throws IOException {
+            Node currentNode = head;
+            for (int i = 0; i < 10; i++) {
+                bw.write(currentNode.data + " ");
+                currentNode = currentNode.next;
+            }
+        }
+    }
     
     public static void main(String[] args) throws IOException {
         for (int t = 1; t < 11; t++) {
-            arrayList = new ArrayList<>();
+            bw.write("#" + t + " ");
             
-            N  = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                arrayList.add(Integer.parseInt(st.nextToken()));
+            LinkedList linkedList = new LinkedList();
+            passwordN   = Integer.parseInt(br.readLine());
+            passwordArr = new int[passwordN];
+            st          = new StringTokenizer(br.readLine());
+            for (int i = 0; i < passwordN; i++) {
+                passwordArr[i] = Integer.parseInt(st.nextToken());
             }
+            linkedList.insert(0, passwordArr);
             
-            Q  = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < Q; i++) {
+            commandN = Integer.parseInt(br.readLine());
+            st       = new StringTokenizer(br.readLine());
+            for (int i = 0; i < commandN; i++) {
                 command = st.nextToken().charAt(0);
                 
                 switch (command) {
                     case 'I':
-                        insert();
+                        commandIndex = Integer.parseInt(st.nextToken());
+                        commandCount = Integer.parseInt(st.nextToken());
+                        commandNumberArr = new int[commandCount];
+                        for (int j = 0; j < commandCount; j++) {
+                            commandNumber       = Integer.parseInt(st.nextToken());
+                            commandNumberArr[j] = commandNumber;
+                        }
+                        linkedList.insert(commandIndex, commandNumberArr);
                         break;
                     case 'D':
-                        delete();
+                        commandIndex = Integer.parseInt(st.nextToken());
+                        commandCount = Integer.parseInt(st.nextToken());
+                        linkedList.delete(commandIndex, commandCount);
                         break;
                     case 'A':
-                        add();
+                        commandCount = Integer.parseInt(st.nextToken());
+                        for (int j = 0; j < commandCount; j++) {
+                            commandNumber = Integer.parseInt(st.nextToken());
+                            linkedList.add(commandNumber);
+                        }
                         break;
                 }
             }
-            bw.write("#" + t);
-            for (int i = 0; i < 10; i++) {
-                bw.write(" " + arrayList.get(i));
-            }
+            linkedList.print();
             bw.write("\n");
         }
         bw.flush();
         bw.close();
-    }
-    
-    static void insert() {
-        commandIndex = Integer.parseInt(st.nextToken());
-        commandCount = Integer.parseInt(st.nextToken());
-        
-        for (int i = 0; i < commandCount; i++) {
-            commandNumber = Integer.parseInt(st.nextToken());
-            arrayList.add(commandIndex, commandNumber);
-            commandIndex++;
-        }
-    }
-    
-    static void delete() {
-        commandIndex = Integer.parseInt(st.nextToken());
-        commandCount = Integer.parseInt(st.nextToken());
-        
-        for (int i = 0; i < commandCount; i++) {
-            arrayList.remove(commandIndex);
-        }
-    }
-    
-    static void add() {
-        commandCount = Integer.parseInt(st.nextToken());
-        
-        for (int i = 0; i < commandCount; i++) {
-            commandNumber = Integer.parseInt(st.nextToken());
-            arrayList.add(commandNumber);
-        }
     }
 }
