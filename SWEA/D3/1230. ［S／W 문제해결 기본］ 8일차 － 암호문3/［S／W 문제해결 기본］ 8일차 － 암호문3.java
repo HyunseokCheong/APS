0413031bc55b4a1e31731916b8d280721fd3a2c2
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-02-11 20:25:18 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 173 ms, Memory: 32,372 KB -BaekjoonHub

--- SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -1,74 +1,95 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.StringTokenizer;
+#include <iostream>
+#include <list>
+#define endl "\n";
+using namespace std;
 
-public class Solution {
+/**
+ * I : insert S at X by Y
+ * D : delete at X by Y
+ * A : add S by Y
+ * @Author HyunseokCheong
+ * @Date 2023/02/11
+ */
+
+int       origin_password_n, command_n, input_int;
+list<int> password;
+char      command;
+int       command_position, command_count, command_number;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     
-    // declare
-    static BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter  bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-    static int             N;
-    static int             Q;
-    static List<Integer>   secretCode;
-    static String          command;
-    static int             X, Y;
-    
-    public static void main(String[] args) throws IOException {
-        for (int t = 1; t < 11; t++) {
-            bw.write("#" + t + " ");
-            N          = Integer.parseInt(br.readLine());
-            st         = new StringTokenizer(br.readLine());
-            secretCode = new LinkedList<>();
-            for (int i = 0; i < N; i++) {
-                secretCode.add(Integer.parseInt(st.nextToken()));
-            }
-            Q     = Integer.parseInt(br.readLine());
-            st    = new StringTokenizer(br.readLine());
-            for (int i = 0; i < Q; i++) {
-                command = st.nextToken();
-                if (command.equals("I")) {
-                    insert(secretCode, st);
-                } else if (command.equals("D")) {
-                    delete(secretCode, st);
-                } else {
-                    add(secretCode, st);
+    for (int test_case = 1; test_case <= 10; test_case++) {
+        
+        password.clear();
+        cin >> origin_password_n;
+        
+        // 원본 암호문 저장
+        for (int i = 0; i < origin_password_n; i++) {
+            
+            cin >> input_int;
+            password.push_back(input_int);
+        }
+        
+        cin >> command_n;
+        
+        for (int i = 0; i < command_n; i++) {
+            
+            cin >> command;
+            
+            if (command == 'I') { // I : insert S at X by Y
+                
+                cin >> command_position >> command_count;
+                auto iterator = password.begin();
+                
+                for (int j = 0; j < command_position; j++) {
+                    iterator++;
+                }
+                
+                for (int j = 0; j < command_count; j++) {
+                    
+                    cin >> command_number;
+                    password.insert(iterator, command_number);
+                }
+                
+            } else if (command == 'D') { // D : delete at X by Y
+                
+                cin >> command_position >> command_count;
+                auto iterator = password.begin();
+                
+                for (int j = 0; j < command_position; j++) {
+                    
+                    iterator++;
+                }
+                
+                for (int j = 0; j < command_count; j++) {
+                    
+                    iterator = password.erase(iterator);
+                }
+                
+            } else if (command == 'A') { //  A : add S by Y
+                
+                cin >> command_count;
+                
+                for (int j = 0; j < command_count; j++) {
+                    
+                    cin >> command_number;
+                    password.push_back(command_number);
                 }
             }
-            for (int i = 0; i < 10; i++) {
-                bw.write(secretCode.get(i) + " ");
-            }
-            bw.write("\n");
         }
-        bw.flush();
-        bw.close();
+        
+        cout << "#" << test_case << " ";
+        
+        for (int i = 0; i < 10; i++) {
+            
+            cout << password.front() << " ";
+            password.pop_front();
+        }
+        cout << endl;
     }
     
-    public static void insert(List<Integer> secretCode, StringTokenizer st) {
-        X = Integer.parseInt(st.nextToken());
-        Y = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < Y; i++) {
-            secretCode.add(X + i, Integer.parseInt(st.nextToken()));
-        }
-    }
-    
-    public static void delete(List<Integer> secretCode, StringTokenizer st) {
-        X = Integer.parseInt(st.nextToken());
-        Y = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < Y; i++) {
-            secretCode.remove(X);
-        }
-    }
-    
-    public static void add(List<Integer> secretCode, StringTokenizer st) {
-        Y = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < Y; i++) {
-            secretCode.add(Integer.parseInt(st.nextToken()));
-        }
-    }
+    return 0;
 }
