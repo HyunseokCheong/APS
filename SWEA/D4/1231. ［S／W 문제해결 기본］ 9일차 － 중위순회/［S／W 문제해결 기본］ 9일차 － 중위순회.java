121d84c96e5f69b75ba93a7af48e09d8231d9820
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-02-13 21:41:59 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 101 ms, Memory: 18,304 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
@@ -5,41 +5,39 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-
 public class Solution {
     
     // declare
-    static BufferedReader  br       = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter  bw       = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter  bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    static int             testCase = 10;
     static int             numberOfNode;
     static char            characterOfNode;
     static char[]          tree;
     
-    // L N R
     static void inOrder(int index) throws IOException {
         if (index > numberOfNode) {
             return;
         }
-        inOrder(2 * index);
+        
+        inOrder(index * 2);
         bw.write(tree[index]);
-        inOrder(2 * index + 1);
+        inOrder(index * 2 + 1);
     }
     
     public static void main(String[] args) throws IOException {
         // read
-        for (int t = 1; t <= testCase; t++) {
+        for (int testCase = 1; testCase < 11; testCase++) {
             numberOfNode = Integer.parseInt(br.readLine());
             tree         = new char[numberOfNode + 1];
             for (int i = 1; i <= numberOfNode; i++) {
                 st = new StringTokenizer(br.readLine());
                 st.nextToken();
-                
                 characterOfNode = st.nextToken().charAt(0);
                 tree[i]         = characterOfNode;
             }
-            bw.write("#" + t + " ");
+            
+            bw.write("#" + testCase + " ");
             inOrder(1);
             bw.write("\n");
         }
