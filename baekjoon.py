fe7ea2fda8576d88b8e779f857929a07dfb12d2e
HyunseokCheong <hyunseokcheong@gmail.com>
2023-04-15 19:47:08 +0900

    230415

--- baekjoon.py
+++ baekjoon.py
@@ -1,1 +1,10 @@
-print(1 if int(input()) + int(input()) <= int(input()) + 0.5 else 0)
+w = int(input())
+l = int(input())
+h = int(input())
+
+answer = 'bad'
+
+if min(l, w) / h >= 2 >= max(l, w) / min(l, w):
+    answer = 'good'
+
+print(answer)
