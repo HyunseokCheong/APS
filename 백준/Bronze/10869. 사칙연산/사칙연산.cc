26e40e06a3683c2cb87c1369a3be0aad4c43a6a8
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-05-02 21:58:22 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.cc
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -1,15 +1,14 @@
 #include <iostream>
-
 using namespace std;
-int main(int argc,char const *argv[]) {
+
+int main()
+{
 	int a, b;
-
 	cin >> a >> b;
-
-	cout << a + b << "\n";
-	cout << a - b << "\n";
-	cout << a * b << "\n";
-	cout << a / b << "\n";
-	cout << a % b << "\n";
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
 	return 0;
 }
