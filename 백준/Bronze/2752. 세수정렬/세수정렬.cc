c6d5e6fcad298a0d9f39d18802edf96cf015ae45
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-05-02 22:06:10 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.cc
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -1,16 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
-    int arr[3];
-    for (int i = 0; i < 3; ++i) {
-        cin >> arr[i];
-    }
-    sort(arr, arr + 3);
+int main()
+{
+	int arr[3];
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> arr[i];
+	}
 
-    for (int i = 0; i < 3; ++i) {
-        cout << arr[i] << " ";
-    }
-    return 0;
+	sort(arr, arr + 3);
+	for (int i = 0; i < 3; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	return 0;
 }
