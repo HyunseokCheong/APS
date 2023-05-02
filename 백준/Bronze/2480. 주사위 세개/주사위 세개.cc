fcb47715d2d129fc34e997ea42666cf3ef5dfb4b
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-05-02 22:14:26 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -1,19 +1,31 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
-    int a, b, c, ans = 0;
-    cin >> a >> b >> c;
-    if (a == b and b == c) {
-        ans = a * 1000 + 10000;
-    } else if (a == b or a == c) {
-        ans = a * 100 + 1000;
-    } else if (b == c) {
-        ans = b * 100 + 1000;
-    } else {
-        ans = max({a, b, c}) * 100;
-    }
-    cout << ans << '\n';
-    return 0;
+int main()
+{
+	int a, b, c;
+	int score;
+	cin >> a >> b >> c;
+
+	if (a == b and b == c)
+	{
+		score = 10000 + a * 1000;
+	}
+	else if (a == b or a == c)
+	{
+		score = 1000 + a * 100;
+	}
+	else if (b == c)
+	{
+		score = 1000 + b * 100;
+	}
+	else
+	{
+		score = max({a, b, c}) * 100;
+	}
+
+	cout << score << endl;
+	return 0;
 }
