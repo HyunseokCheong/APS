f25db4a54ab3cae0ae0a945cc5b7a10621100828
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-05-02 22:37:55 +0900

    [Bronze II] Title: 숫자, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.cc
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -1,23 +1,31 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#define endl "\n"
 
 using namespace std;
 
-int main() {
-    long long a, b;
-    cin >> a >> b;
+int main()
+{
+	long long a, b;
+	cin >> a >> b;
 
-    if (a > b) {
-        swap(a, b);
-    }
+	if (a > b)
+	{
+		swap(a, b);
+	}
 
-    if (a == b or b - a == 1) {
-        cout << 0;
-        return 0;
-    } else {
-        cout << b - a - 1 << '\n';
-        for (long long i = a + 1; i < b; i++) {
-            cout << i << ' ';
-        }
-        return 0;
-    }
+	if (a != b)
+	{
+		cout << b - a - 1 << endl;
+		for (long long i = a + 1; i < b; i++)
+		{
+			cout << i << " ";
+		}
+	}
+	else
+	{
+		cout << 0;
+	}
+
+	cout << endl;
+	return 0;
 }
