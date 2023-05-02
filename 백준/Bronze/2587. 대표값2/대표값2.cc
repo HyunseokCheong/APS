9f437b26813ef2cb0119d3c301e7b1bea0635b24
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-05-02 22:22:28 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.cc
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -1,15 +1,21 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main() {
-    int arr[5];
-    int sum = 0;
-    for (int i = 0; i < 5; ++i) {
-        cin >> arr[i];
-        sum += arr[i];
-    }
-    sort(arr, arr + 5);
-    cout << sum / 5 << '\n' << arr[2];
-    return 0;
+int main()
+{
+	int arr[5];
+	int sum = 0;
+	int average;
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> arr[i];
+		sum += arr[i];
+	}
+
+	sort(arr, arr + 5);
+	average = sum / 5;
+	cout << average << endl
+		 << arr[2] << endl;
+	return 0;
 }
