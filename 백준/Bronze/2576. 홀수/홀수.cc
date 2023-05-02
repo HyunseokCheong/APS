4d6f4f78aed1513a9620be94f09ac8948b574066
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-05-02 22:19:42 +0900

    [Bronze III] Title: 홀수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.cc
+++ 백준/Bronze/2576. 홀수/홀수.cc
@@ -1,22 +1,30 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 
-int main() {
-    int n, min = 100, sum = 0;
-    for (int i = 0; i < 7; i++) {
-        cin >> n;
-        if (n % 2 == 1) {
-            sum += n;
-            if (n < min) {
-                min = n;
-            }
-        }
-    }
-    if (sum == 0) {
-        cout << -1;
-    } else {
-        cout << sum << '\n' << min;
-    }
-    return 0;
+int main()
+{
+	int inputNumber;
+	int sumOfOddNumber = 0;
+	int minValueOfOddNumber = 100;
+
+	for (int i = 0; i < 7; i++)
+	{
+		cin >> inputNumber;
+		if (inputNumber % 2 != 0)
+		{
+			sumOfOddNumber += inputNumber;
+			minValueOfOddNumber = min(minValueOfOddNumber, inputNumber);
+		}
+	}
+
+	if (sumOfOddNumber == 0)
+	{
+		cout << -1 << endl;
+	}
+	else
+	{
+		cout << sumOfOddNumber << endl
+			 << minValueOfOddNumber << endl;
+	}
+	return 0;
 }
