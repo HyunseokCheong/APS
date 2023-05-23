c0159198eacc05fd2075feddba378162c985995c
HyunseokCheong <hyunseokcheong@gmail.com>
2023-05-23 11:40:52 +0900

    230523

--- Main.java
+++ Main.java
@@ -3,32 +3,38 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main {
-
-    static BufferedReader  br    = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter  bw    = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-    static String          endl  = "\n";
-    static String          blank = " ";
-
-
+    
+    static BufferedReader br   = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw   = new BufferedWriter(new OutputStreamWriter(System.out));
+    static String         endl = "\n";
+    static int            N;
+    static int[][]        arr  = {{2, 3, 5, 7}, {23, 29, 31, 37, 53, 59, 71, 73, 79},
+            {233, 239, 293, 311, 313, 317, 373, 379, 593, 599, 719, 733, 739, 797},
+            {2333, 2339, 2393, 2399, 2939, 3119, 3137, 3733, 3739, 3793, 3797, 5939, 7193, 7331,
+                    7333, 7393},
+            {23333, 23339, 23399, 23993, 29399, 31193, 31379, 37337, 37339, 37397, 59393, 59399,
+                    71933, 73331, 73939},
+            {233993, 239933, 293999, 373379, 373393, 593933, 593993, 719333, 739391, 739393, 739397,
+                    739399},
+            {2339933, 2399333, 2939999, 3733799, 5939333, 7393913, 7393931, 7393933},
+            {23399339, 29399999, 37337999, 59393339, 73939133}};
+    
     static void input() throws IOException {
+        N = Integer.parseInt(br.readLine());
     }
-
+    
     static void process() throws IOException {
-
+        for (int num : arr[N - 1]) {
+            bw.write(num + endl);
+        }
         bw.flush();
         bw.close();
     }
-
+    
     public static void main(String[] args) throws IOException {
         input();
         process();
-    }
-
-    static void stk() throws IOException {
-        st = new StringTokenizer(br.readLine());
     }
 }
