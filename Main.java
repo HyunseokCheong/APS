9f1aeff9d17c3838638d6a8a8b8a1d573de6f31e
HyunseokCheong <hyunseokcheong@gmail.com>
2023-05-26 13:58:13 +0900

    230526

--- Main.java
+++ Main.java
@@ -3,59 +3,31 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main {
     
-    static BufferedReader  br    = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter  bw    = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-    static String          endl  = "\n";
-    static String          blank = " ";
-    static int             point;
-    static int[]           arr;
+    static BufferedReader br   = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw   = new BufferedWriter(new OutputStreamWriter(System.out));
+    static String         endl = "\n";
+    static int            N;
+    static int            M;
+    static String         color;
     
     static void input() throws IOException {
-        stk();
-        arr   = new int[3];
-        point = 0;
-        for (int i = 0; i < 3; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            point += arr[i];
-        }
-    }
-    
-    static char findUnique(int target) {
-        int findIndex = 0;
-        for (int i = 0; i < 3; i++) {
-            if (arr[i] == target) {
-                findIndex = i;
-                break;
-            }
-        }
-        switch (findIndex) {
-            case 0:
-                return 'A';
-            case 1:
-                return 'B';
-            case 2:
-                return 'C';
-        }
-        return ' ';
-    }
-    
-    static char getAnswer(int point) {
-        if (point == 0 || point == 3) {
-            return '*';
-        }
-        if (point == 1) {
-            return findUnique(1);
-        }
-        return findUnique(0);
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
     }
     
     static void process() throws IOException {
-        bw.write(getAnswer(point) + endl);
+        if (50 >= N && 10 >= M) {
+            color = "White";
+        } else if (30 < M) {
+            color = "Red";
+        } else {
+            color = "Yellow";
+        }
+        
+        bw.write(color);
         bw.flush();
         bw.close();
     }
@@ -63,9 +35,5 @@ public class Main {
     public static void main(String[] args) throws IOException {
         input();
         process();
-    }
-    
-    static void stk() throws IOException {
-        st = new StringTokenizer(br.readLine());
     }
 }
