ab6a43b85066ab2facf5d25359c2d1dc303d1019
HyunseokCheong <hyunseokcheong@gmail.com>
2023-06-03 09:20:30 +0900

    230603

--- SOFTEER/lv1/근무시간.py
+++
@@ -1,8 +0,0 @@
-t = 0
-for _ in range(5):
-    s, e = input().split()
-    h, m = map(int, e.split(':'))
-    t += h * 60 + m
-    h, m = map(int, s.split(':'))
-    t -= h * 60 + m
-print(t)
--- SOFTEER/lv2/바이러스.py
+++
@@ -1,6 +0,0 @@
-k, p, n = map(int, input().split())
-
-for i in range(n):
-    k = k * p % 1000000007
-
-print(k)
