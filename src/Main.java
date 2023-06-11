853384a8993eee91ca167dcf0e0a73c37f427f8f
hyunseokcheong <hyunseokcheong@gmail.com>
2023-06-11 23:09:06 +0900

    230611

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,39 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    static int n, k;
+    // declare
+    static int[] arr;
+    static int count, answer;
 
     public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            k = Integer.parseInt(br.readLine());
-            for (int j = 0; j < k; j++) {
-                bw.write('=' + "");
-            }
-            bw.newLine();
+        // read
+        st = new StringTokenizer(br.readLine());
+        arr = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        // solve
+        for (int i = 1; i < 1000001; i++) {
+            count = 0;
+            for (int j = 0; j < 5; j++) {
+                if (i % arr[j] == 0) {
+                    count++;
+                }
+            }
+            if (count >= 3) {
+                answer = i;
+                break;
+            }
+        }
+
+        // write
+        bw.write(answer + " ");
         bw.flush();
         bw.close();
     }
