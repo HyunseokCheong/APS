dff8198cd1335b0c38204364854152f64f3c7fcc
hyunseokcheong <hyunseokcheong@gmail.com>
2023-06-11 22:41:57 +0900

    230611

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
-    
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int n, k;
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            k = Integer.parseInt(br.readLine());
+            for (int j = 0; j < k; j++) {
+                bw.write('=' + "");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
     }
 }
