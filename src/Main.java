f9d3221bfc0b8e17cd9d4b71725a44cf418d2311
hyunseokcheong <hyunseokcheong@gmail.com>
2023-06-12 09:15:25 +0900

    230612

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,45 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    // declare
     static int[] arr;
-    static int count, answer;
 
     public static void main(String[] args) throws IOException {
-        // read
-        st = new StringTokenizer(br.readLine());
-        arr = new int[5];
-        for (int i = 0; i < 5; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        arr = new int[9];
+        int sum = 0;
+        int fakeIndex1 = -1;
+        int fakeIndex2 = -1;
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
         }
+        Arrays.sort(arr);
 
-        // solve
-        for (int i = 1; i < 1000001; i++) {
-            count = 0;
-            for (int j = 0; j < 5; j++) {
-                if (i % arr[j] == 0) {
-                    count++;
+        loop1:
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (sum - arr[i] - arr[j] == 100) {
+                    fakeIndex1 = i;
+                    fakeIndex2 = j;
+                    break loop1;
                 }
             }
-            if (count >= 3) {
-                answer = i;
-                break;
-            }
         }
 
-        // write
-        bw.write(answer + " ");
+        for (int i = 0; i < 9; i++) {
+            if (i == fakeIndex1 || i == fakeIndex2) {
+                continue;
+            }
+            bw.write(arr[i] + "\n");
+        }
         bw.flush();
         bw.close();
     }
