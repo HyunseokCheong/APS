6751dc496f4d8d785b5d757b456631c614533bc7
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-06-25 01:58:35 +0900

    Delete 백준 directory

--- 백준/Bronze/28097. 모범생 포닉스/모범생 포닉스.py
+++
@@ -1,4 +0,0 @@
-n = int(input())
-arr = list(map(int, input().split()))
-answer = (n - 1) * 8 + sum(arr)
-print(answer // 24, answer % 24)
--- 백준/Bronze/28224. Final Price/Final Price.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-answer = 0
-for i in range(n):
-    answer += int(input())
-print(answer)
