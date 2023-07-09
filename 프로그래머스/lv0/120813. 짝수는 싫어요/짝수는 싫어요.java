1afb2757776bec30bb8cd07fc83d05de34a9ffc7
HyunseokCheong <109321543+HyunseokCheong@users.noreply.github.com>
2023-07-09 22:15:44 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.44 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                list.add(i);
+            }
+        }
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
