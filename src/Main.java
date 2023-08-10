be08fafa2b3a8899205c26623ab9d169230a3f11
HyunseokCheong <hyunseokcheong@gmail.com>
2023-08-10 12:08:25 +0900

    230810

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < N; i++) {
+            min = Math.min(min, arr[i] + arr[i - 1]);
+        }
+        
+        long answer = (long) min * X;
+        bw.write(answer + "\n");
+        bw.flush();
+        bw.close();
     }
 }
