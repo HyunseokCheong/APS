bf7a2c5b302e708063cc98a744d822e931deebf9
HyunseokCheong <hyunseokcheong@gmail.com>
2023-08-19 18:22:47 +0900

    230819

---
+++ src/atcoder/abc_315/a/Main.java
@@ -0,0 +1,30 @@
+package atcoder.abc_315.a;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    
+    
+    static void input() throws IOException {
+    
+    }
+    
+    static void process() throws IOException {
+    
+    }
+    
+    static void output() throws IOException {
+    
+    }
+    
+    public static void main(String[] args) throws IOException {
+        input();
+        process();
+        output();
+    }
+}
