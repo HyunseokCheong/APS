b80a164193e31ae47bf91b05144c7e411ccdbdad
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2023-08-25 08:32:03 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 76 ms, Memory: 11644 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    
+    public static void main(String[] args) throws IOException {
+        int sum = Integer.MAX_VALUE;
+        st = new StringTokenizer(br.readLine(), "-");
+        while (st.hasMoreTokens()) {
+            int cur = 0;
+            StringTokenizer st2 = new StringTokenizer(st.nextToken(), "+");
+            
+            while (st2.hasMoreTokens()) {
+                cur += Integer.parseInt(st2.nextToken());
+            }
+            
+            if (sum == Integer.MAX_VALUE) {
+                sum = cur;
+            } else {
+                sum -= cur;
+            }
+        }
+        
+        bw.write(sum + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
