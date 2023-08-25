298836f832bedaf963d22e0a0775725acf6a14c9
HyunseokCheong <hyunseokcheong@gmail.com>
2023-08-26 07:57:19 +0900

    230826

---
+++ src/streak/BOJ_9325.java
@@ -0,0 +1,30 @@
+package streak;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_9325 {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int testCase, s, n, q, p;
+    
+    public static void main(String[] args) throws IOException {
+        testCase = Integer.parseInt(br.readLine());
+        while (testCase-- > 0) {
+            s = Integer.parseInt(br.readLine());
+            n = Integer.parseInt(br.readLine());
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                q = Integer.parseInt(st.nextToken());
+                p = Integer.parseInt(st.nextToken());
+                s += q * p;
+            }
+            
+            bw.write(s + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
