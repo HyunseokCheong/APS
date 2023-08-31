ccbd83cd67154560fa78c1c4b23ff3dd0d2af721
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2023-08-31 14:55:51 +0900

    clear

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int solution(int[] box, int n) {
-        int answer = 1;
-        for (int number : box) {
-            answer *= number / n;
-        }
-        return answer;
-    }
-}
--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int solution(String number) {
-        int answer = 0;
-        for (char c : number.toCharArray()) {
-            answer += Integer.parseInt(String.valueOf(c));
-        }
-        return answer % 9;
-    }
-}
