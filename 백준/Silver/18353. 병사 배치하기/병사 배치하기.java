f25c7a67066852d57401ce869ffd0e39f4e4cc5b
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2023-10-23 09:12:16 +0900

    [Silver II] Title: 병사 배치하기, Time: 176 ms, Memory: 15768 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -1,29 +1,34 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * 가장 긴 증가 수열을 찾고, 해당 수열에 포함되지 않는 숫자를 구한다.
+ * 배열의 길이 - 가장 긴 즐가 수열
+ */
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
     static int n, result;
-    static int[] soldier, dp;
+    static int[] soldiers, dp;
     
     static void input() throws IOException {
         n = Integer.parseInt(br.readLine());
-        soldier = new int[n + 1];
+        soldiers = new int[n + 1];
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i < n + 1; i++) {
-            soldier[i] = Integer.parseInt(st.nextToken());
+            soldiers[i] = Integer.parseInt(st.nextToken());
         }
     }
     
     static void process() throws IOException {
         result = 0;
         dp = new int[n + 1];
+        Arrays.fill(dp, 1);
         for (int i = 1; i < n + 1; i++) {
-            dp[i] = 1;
             for (int j = 1; j < i; j++) {
-                if (soldier[j] > soldier[i]) {
+                if (soldiers[j] > soldiers[i]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
