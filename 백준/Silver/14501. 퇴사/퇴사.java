5924f8d2d965048a28b1073321cb8680a3b4bc2e
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2023-11-02 12:49:58 +0900

    [Silver III] Title: 퇴사, Time: 144 ms, Memory: 15904 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -6,40 +6,38 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
     static int n;
-    static int[] memo;
     static int[][] schedule;
+    static int maxCost;
     
     static void input() throws IOException {
         n = Integer.parseInt(br.readLine());
         schedule = new int[n][2];
-        memo = new int[n];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             schedule[i][0] = Integer.parseInt(st.nextToken());
             schedule[i][1] = Integer.parseInt(st.nextToken());
-            memo[i] = -1;
         }
     }
     
-    static int recur(int day) {
-        if (day >= n) {
-            return 0;
+    static void backtracking(int day, int cost) {
+        if (day == n) {
+            maxCost = Math.max(maxCost, cost);
+            return;
         }
-        if (memo[day] != -1) {
-            return memo[day];
-        }
+        
         int nextDay = day + schedule[day][0];
-        int takeCost = 0;
+        int nextCost = cost + schedule[day][1];
+        
         if (nextDay <= n) {
-            takeCost = schedule[day][1] + recur(nextDay);
+            backtracking(nextDay, nextCost);
         }
-        int skipCost = recur(day + 1);
-        memo[day] = Math.max(takeCost, skipCost);
-        return memo[day];
+        backtracking(day + 1, cost);
     }
     
     static void process() throws IOException {
-        bw.write(recur(0) + "\n");
+        maxCost = 0;
+        backtracking(0, 0);
+        bw.write(maxCost + "\n");
     }
     
     static void output() throws IOException {
