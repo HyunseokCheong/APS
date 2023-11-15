6141a9e4c9d781d1be07148f24aa63828772703c
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2023-11-15 11:12:21 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 45664 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,37 +1,22 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int n;
-    static Queue<Integer> queue;
     
-    static void input() throws IOException {
-        n = Integer.parseInt(br.readLine());
-    }
-    
-    static void process() throws IOException {
-        queue = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
         for (int i = 1; i < n + 1; i++) {
             queue.add(i);
         }
-        while (queue.size() != 1) {
+        while (queue.size() > 1) {
             queue.poll();
             queue.add(queue.poll());
         }
         bw.write(queue.poll() + "\n");
-    }
-    
-    static void output() throws IOException {
         bw.flush();
         bw.close();
-    }
-    
-    public static void main(String[] args) throws IOException {
-        input();
-        process();
-        output();
     }
 }
