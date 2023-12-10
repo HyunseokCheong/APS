0b36321d8d67657f3f177f2e82625f6e9c29b96b
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2023-12-10 10:01:19 +0900

    [Silver IV] Title: Sort 마스터 배지훈의 후계자, Time: 1204 ms, Memory: 82052 KB -BaekjoonHub

--- 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.java
+++ 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.java
@@ -6,15 +6,14 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    static int n, m, d;
-    static int[] arr;
+    static int n, m;
     static Map<Integer, Integer> numIdxMap;
+    static int[] arr;
     
     static void input() throws IOException {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        numIdxMap = new TreeMap<>();
         arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
@@ -22,20 +21,19 @@ public class Main {
     }
     
     static void process() throws IOException {
+        numIdxMap = new TreeMap<>();
         Arrays.sort(arr);
         for (int i = 0; i < n; i++) {
-            if (!numIdxMap.containsKey(arr[i])) {
-                numIdxMap.put(arr[i], i);
+            int c = arr[i];
+            if (numIdxMap.containsKey(c)) {
+                continue;
             }
+            numIdxMap.put(c, i);
         }
+        
         for (int i = 0; i < m; i++) {
-            d = Integer.parseInt(br.readLine());
-            if (!numIdxMap.containsKey(d)) {
-                bw.write(-1 + "\n");
-            }
-            if (numIdxMap.containsKey(d)) {
-                bw.write(numIdxMap.get(d) + "\n");
-            }
+            int d = Integer.parseInt(br.readLine());
+            bw.write(numIdxMap.getOrDefault(d, -1) + "\n");
         }
     }
     
