05ba31c5061ed20320454cd4ef6df65af1b068d6
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2024-01-04 18:52:31 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 5060 ms, Memory: 138020 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,7 @@
+store = {}
+n, m = map(int, input().split())
+for i in range(n):
+    address, password = input().split()
+    store[address] = password
+for i in range(m):
+    print(store[input()])
