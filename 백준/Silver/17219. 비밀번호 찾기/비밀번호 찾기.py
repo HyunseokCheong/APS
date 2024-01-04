7ec1cd206790949cb69f479a2d932246a3ae4671
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2024-01-04 18:54:57 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 332 ms, Memory: 135068 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,7 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
 store = {}
 n, m = map(int, input().split())
 for i in range(n):
     address, password = input().split()
     store[address] = password
 for i in range(m):
-    print(store[input()])
+    print(store[input().rstrip()])
