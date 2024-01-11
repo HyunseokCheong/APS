e9729c86ebb7ef558a722c19e9c949313fe21965
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2024-01-11 14:35:51 +0900

    [Gold V] Title: 암호 만들기, Time: 160 ms, Memory: 114592 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,36 +1,48 @@
-def check(string):
-    mo = 0
-    ja = 0
-    vowel = ['a', 'e', 'i', 'o', 'u']
-    for i in string:
-        flag = False
-        for j in vowel:
-            if i == j:
-                mo += 1
-                flag = True
-                break
-        if flag:
+from collections import deque
+
+
+def is_ok(password: str) -> bool:
+    # 3 글자 이상
+    if len(password) < 3:
+        return False
+
+    # 모음 1개, 자음 2개 이상
+    vowel = {'a', 'e', 'i', 'o', 'u'}
+    vowel_cnt = 0
+    for p in password:
+        if p in vowel:
+            vowel_cnt += 1
+    if vowel_cnt < 1 or len(password) - vowel_cnt < 2:
+        return False
+
+    # 오름차순
+    for i in range(len(password) - 1):
+        if password[i] > password[i + 1]:
+            return False
+
+    # 모든 조건 만족
+    return True
+
+
+def bfs():
+    queue = deque()
+    queue.append(["", 0])  # password, index
+
+    while queue:
+        cur_value = queue.popleft()
+        password = cur_value[0]
+        start_idx = cur_value[1]
+
+        if len(password) == l and is_ok(password):
+            print(password)
             continue
-        ja += 1
 
-    return mo >= 1 and ja >= 2
-
-
-def solve(string, depth, start):
-    if depth == l:
-        if check(string):
-            print(string)
-        return
-    for i in range(start, c):
-        if visited[i]:
-            continue
-        visited[i] = True
-        solve(string + arr[i], depth + 1, i + 1)
-        visited[i] = False
+        for i in range(start_idx, c):
+            next_password = password + data[i]
+            next_idx = i + 1
+            queue.append([next_password, next_idx])
 
 
 l, c = map(int, input().split())
-arr = sorted(list(input().split()))
-visited = [False] * c
-
-solve("", 0, 0)
+data = sorted(list(input().split()))
+bfs()
