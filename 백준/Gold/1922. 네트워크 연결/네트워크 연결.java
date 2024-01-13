beeee5e4e13aba83815645ea5276af9892746d0f
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2024-01-13 13:04:09 +0900

    [Gold IV] Title: 네트워크 연결, Time: 572 ms, Memory: 49080 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,73 +1,72 @@
 import java.io.*;
-import java.util.*;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
     
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    static int n, m, result;
-    static List<Edge>[] tree;
-    static boolean[] visited;
-    static PriorityQueue<Edge> edges;
+    static int[] parent;
     
-    static class Edge {
+    static int find(int x) {
+        if (x != parent[x]) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    
+    static void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
         
-        int vertex;
-        int weight;
-        
-        public Edge(int vertex, int weight) {
-            this.vertex = vertex;
-            this.weight = weight;
+        if (rootX < rootY) {
+            parent[rootY] = rootX;
+        } else if (rootX > rootY) {
+            parent[rootX] = rootY;
         }
     }
     
-    static void input() throws IOException {
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-        tree = new List[n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            tree[i] = new ArrayList<>();
-        }
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        PriorityQueue<Edge> edges = new PriorityQueue<>((e1, e2) -> e1.weight - e2.weight);
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int vertex1 = Integer.parseInt(st.nextToken());
             int vertex2 = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
-            tree[vertex1].add(new Edge(vertex2, weight));
-            tree[vertex2].add(new Edge(vertex1, weight));
+            edges.add(new Edge(vertex1, vertex2, weight));
         }
-    }
-    
-    static void process() throws IOException {
-        result = 0;
-        edges = new PriorityQueue<>((e1, e2) -> e1.weight - e2.weight);
-        visited = new boolean[n + 1];
         
-        edges.addAll(tree[1]);
-        visited[1] = true;
+        parent = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            parent[i] = i;
+        }
         
+        int result = 0;
         while (!edges.isEmpty()) {
-            Edge curEdge = edges.poll();
-            if (visited[curEdge.vertex]) {
-                continue;
+            Edge edge = edges.poll();
+            if (find(edge.vertex1) != find(edge.vertex2)) {
+                union(edge.vertex1, edge.vertex2);
+                result += edge.weight;
             }
-            visited[curEdge.vertex] = true;
-            result += curEdge.weight;
-            
-            edges.addAll(tree[curEdge.vertex]);
         }
-    }
-    
-    static void output() throws IOException {
         bw.write(result + "\n");
         bw.flush();
         bw.close();
     }
     
-    public static void main(String[] args) throws IOException {
-        input();
-        process();
-        output();
+    static class Edge {
+        
+        int vertex1;
+        int vertex2;
+        int weight;
+        
+        public Edge(int vertex1, int vertex2, int weight) {
+            this.vertex1 = vertex1;
+            this.vertex2 = vertex2;
+            this.weight = weight;
+        }
     }
 }
