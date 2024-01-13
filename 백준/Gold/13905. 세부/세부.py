b39eb30ee54c919cdbff257794bcb7c64bc587ae
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2024-01-13 17:27:31 +0900

    [Gold IV] Title: 세부, Time: 884 ms, Memory: 85784 KB -BaekjoonHub

--- 백준/Gold/13905. 세부/세부.py
+++ 백준/Gold/13905. 세부/세부.py
@@ -1,41 +1,36 @@
-import heapq
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = list(range(n + 1))
 
+    def find(self, x):
+        if self.parent[x] == x:
+            return x
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
 
-def find(x: int) -> int:
-    if x != parent[x]:
-        parent[x] = find(parent[x])
-    return parent[x]
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x == root_y:
+            return
+        self.parent[root_x] = root_y
 
+if __name__ == '__main__':
+    from sys import stdin, setrecursionlimit
+    input = stdin.readline
+    setrecursionlimit(10 ** 7)
 
-def union(x: int, y: int) -> None:
-    root_x = find(x)
-    root_y = find(y)
-
-    if root_x < root_y:
-        parent[root_y] = root_x
-    elif root_x > root_y:
-        parent[root_x] = root_y
-
-
-if __name__ == "__main__":
     n, m = map(int, input().split())
     s, e = map(int, input().split())
-
-    edges = []
+    bridge = []
     for _ in range(m):
-        v1, v2, w = map(int, input().split())
-        heapq.heappush(edges, (-w, v1, v2))
-
-    parent = [i for i in range(n + 1)]
-
-    result = 0
-    while edges:
-        w, v1, v2 = heapq.heappop(edges)
-
-        if find(v1) != find(v2):
-            union(v1, v2)
-
-            if find(s) == find(e):
-                result = -w
-                break
-    print(result)
+        x, y, k = map(int, input().split())
+        bridge.append((k, x, y))
+    bridge.sort()
+    djs = DisjointSet(n)
+    last = 0
+    while djs.find(s) != djs.find(e) and bridge:
+        d, x, y = bridge.pop()
+        last = d
+        djs.union(x, y)
+    print(last if djs.find(s) == djs.find(e) else 0)
