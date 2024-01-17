8bf2fb6050b9a501a081b51bf9f6b34bf0e3d98b
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2024-01-17 15:29:12 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 656 ms, Memory: 159984 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,11 +1,17 @@
 import sys
+
 input = sys.stdin.readline
 
-n = int(input())
-arr = [0] * n
-for i in range(n):
-    arr[i] = int(input())
-arr.sort()
+if __name__ == "__main__":
+    """
+    1 <= n <= 1,000,000
+    abs(ni) <= 1,000,000
+    """
+    n = int(input())
+    arr = [0] * n
+    for i in range(n):
+        arr[i] = int(input())
 
-for i in arr:
-    print(i)
+    arr.sort()
+
+    print(*arr, sep="\n")
