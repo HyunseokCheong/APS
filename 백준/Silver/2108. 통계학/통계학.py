8672c989248550e87c7433bee86fa85a7ee93c49
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2024-01-17 15:43:33 +0900

    [Silver III] Title: 통계학, Time: 216 ms, Memory: 110596 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,14 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
 if __name__ == "__main__":
+    """
+    1 <= n <= 500,000
+    n % 2 == 1
+    abs(arr[i]) <= 4,000
+    """
     n = int(input())
     r1 = 0
     max_value = -4000
     min_value = 4000
     count = [0] * 8001
+    max_count = 0
 
     for i in range(n):
         num = int(input())
         r1 += num
         count[num + 4000] += 1
+        max_count = max(max_count, count[num + 4000])
         max_value = max(max_value, num)
         min_value = min(min_value, num)
 
@@ -16,8 +27,8 @@ if __name__ == "__main__":
     r1 = round(r1 / n)
 
     # 중앙값
-    cnt = 0
     r2 = 0
+    cnt = 0
     for i in range(8001):
         if count[i] != 0:
             cnt += count[i]
@@ -26,9 +37,8 @@ if __name__ == "__main__":
                 break
 
     # 최빈값
-    max_count = max(count)
-    mode_candidates = [i - 4000 for i, c in enumerate(count) if c == max_count]
-    r3 = mode_candidates[0] if len(mode_candidates) == 1 else mode_candidates[1]
+    r3_candidate = [i - 4000 for i, c in enumerate(count) if c == max_count]
+    r3 = r3_candidate[0] if len(r3_candidate) == 1 else r3_candidate[1]
 
     # 범위
     r4 = max_value - min_value
