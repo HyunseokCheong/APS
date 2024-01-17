2e88b8654a7ca4fbb849e58311b2d4295693b450
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2024-01-17 19:07:43 +0900

    [Silver V] Title: 나이순 정렬, Time: 328 ms, Memory: 127740 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,20 +1,10 @@
-import sys
+n = int(input())
+people = []
+for _ in range(n):
+    age, name = input().split()
+    people.append((int(age), name))
 
-input = sys.stdin.readline
+people.sort(key=lambda x:(x[0]))
 
-if __name__ == "__main__":
-    """
-    1 <= n <= 100,000
-    1 <= age <= 200
-    len(name) <= 100
-    """
-    n = int(input())
-    arr = []
-    for idx in range(n):
-        age, name = input().split()
-        arr.append([int(age), idx, name])
-
-    arr.sort(key=lambda x: (x[0], x[1]))
-
-    for age, idx, name in arr:
-        print(age, name)
+for person in people:
+    print(person[0], person[1])
