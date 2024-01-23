4b87001a172323397be31dbccb6f23e722426015
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2024-01-23 12:15:17 +0900

    [Silver IV] Title: 덱, Time: 240 ms, Memory: 19936 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -2,60 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    static int n, result;
-    static String op;
-    static Deque<Integer> deque;
     
-    static void input() throws IOException {
-        n = Integer.parseInt(br.readLine());
-    }
-    
-    static void process() throws IOException {
-        deque = new LinkedList<>();
-        while (n-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            op = st.nextToken();
-            switch (op) {
-                case "push_front":
-                    deque.addFirst(Integer.parseInt(st.nextToken()));
-                    continue;
-                case "push_back":
-                    deque.addLast(Integer.parseInt(st.nextToken()));
-                    continue;
-                case "pop_front":
-                    result = deque.isEmpty() ? -1 : deque.pollFirst();
-                    break;
-                case "pop_back":
-                    result = deque.isEmpty() ? -1 : deque.pollLast();
-                    break;
-                case "size":
-                    result = deque.size();
-                    break;
-                case "empty":
-                    result = deque.isEmpty() ? 1 : 0;
-                    break;
-                case "front":
-                    result = deque.isEmpty() ? -1 : deque.peekFirst();
-                    break;
-                case "back":
-                    result = deque.isEmpty() ? -1 : deque.peekLast();
-                    break;
-            }
-            bw.write(result + "\n");
-        }
-    }
-    
-    static void output() throws IOException {
-        bw.flush();
-        bw.close();
-    }
     
     public static void main(String[] args) throws IOException {
-        input();
-        process();
-        output();
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> deque = new LinkedList<>();
+        int x;
+        while (n-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            String operation = st.nextToken();
+            switch (operation) {
+                case "push_front":
+                    x = Integer.parseInt(st.nextToken());
+                    deque.addFirst(x);
+                    break;
+                case "push_back":
+                    x = Integer.parseInt(st.nextToken());
+                    deque.addLast(x);
+                    break;
+                case "pop_front":
+                    bw.write((deque.isEmpty() ? -1 : deque.pollFirst()) + "\n");
+                    break;
+                case "pop_back":
+                    bw.write((deque.isEmpty() ? -1 : deque.pollLast()) + "\n");
+                    break;
+                case "size":
+                    bw.write(deque.size() + "\n");
+                    break;
+                case "empty":
+                    bw.write((deque.isEmpty() ? 1 : 0) + "\n");
+                    break;
+                case "front":
+                    bw.write((deque.isEmpty() ? -1 : deque.peekFirst()) + "\n");
+                    break;
+                case "back":
+                    bw.write((deque.isEmpty() ? -1 : deque.peekLast()) + "\n");
+                    break;
+            }
+        }
+        bw.flush();
+        bw.close();
     }
 }
