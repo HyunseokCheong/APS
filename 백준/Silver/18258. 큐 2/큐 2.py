4aea586961fe27cbca38391ae1b69dea63b51306
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2024-01-23 13:20:04 +0900

    [Silver IV] Title: 큐 2, Time: 872 ms, Memory: 164268 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -3,32 +3,20 @@ from collections import deque
 
 input = sys.stdin.readline
 
-n = int(input())
-q = deque()
-for _ in range(n):
-    cmd = list(input().split())
-
-    if cmd[0] == "push":
-        q.append(cmd[1])
-    elif cmd[0] == "pop":
-        if len(q) != 0:
-            print(q.popleft())
-        else:
-            print(-1)
-    elif cmd[0] == "size":
-        print(len(q))
-    elif cmd[0] == "empty":
-        if len(q) == 0:
-            print(1)
-        else:
-            print(0)
-    elif cmd[0] == "front":
-        if len(q) != 0:
-            print(q[0])
-        else:
-            print(-1)
-    elif cmd[0] == "back":
-        if len(q) != 0:
-            print(q[-1])
-        else:
-            print(-1)
+if __name__ == "__main__":
+    n = int(input())
+    queue = deque()
+    for _ in range(n):
+        cmd = list(input().split())
+        if cmd[0] == "push":
+            queue.append(int(cmd[1]))
+        elif cmd[0] == 'pop':
+            print(-1 if not queue else queue.popleft())
+        elif cmd[0] == "size":
+            print(len(queue))
+        elif cmd[0] == "empty":
+            print(1 if not queue else 0)
+        elif cmd[0] == "front":
+            print(-1 if not queue else queue[0])
+        elif cmd[0] == "back":
+            print(-1 if not queue else queue[-1])
