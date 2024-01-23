6aa3c63fb72201082a1ee64c16ff31a76659833a
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2024-01-23 13:29:54 +0900

    [Silver IV] Title: 덱, Time: 192 ms, Memory: 115376 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -3,38 +3,24 @@ from collections import deque
 
 input = sys.stdin.readline
 
-dq = deque()
-n = int(input())
-for _ in range(n):
-    cmd = list(input().split())
-    if cmd[0] == 'push_front':
-        dq.appendleft(cmd[1])
-    elif cmd[0] == 'push_back':
-        dq.append(cmd[1])
-    elif cmd[0] == 'pop_front':
-        if not dq:
-            print(-1)
-        else:
-            print(dq.popleft())
-    elif cmd[0] == 'pop_back':
-        if not dq:
-            print(-1)
-        else:
-            print(dq.pop())
-    elif cmd[0] == 'size':
-        print(len(dq))
-    elif cmd[0] == 'empty':
-        if not dq:
-            print(1)
-        else:
-            print(0)
-    elif cmd[0] == 'front':
-        if not dq:
-            print(-1)
-        else:
-            print(dq[0])
-    elif cmd[0] == 'back':
-        if not dq:
-            print(-1)
-        else:
-            print(dq[-1])
+if __name__ == "__main__":
+    deque = deque()
+    n = int(input())
+    for _ in range(n):
+        cmd = list(input().split())
+        if cmd[0] == 'push_front':
+            deque.appendleft(cmd[1])
+        elif cmd[0] == 'push_back':
+            deque.append(cmd[1])
+        elif cmd[0] == 'pop_front':
+            print(-1 if not deque else deque.popleft())
+        elif cmd[0] == 'pop_back':
+            print(-1 if not deque else deque.pop())
+        elif cmd[0] == 'size':
+            print(len(deque))
+        elif cmd[0] == 'empty':
+            print(1 if not deque else 0)
+        elif cmd[0] == 'front':
+            print(-1 if not deque else deque[0])
+        elif cmd[0] == 'back':
+            print(-1 if not deque else deque[-1])
