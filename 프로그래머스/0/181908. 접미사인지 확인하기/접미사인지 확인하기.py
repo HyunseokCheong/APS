a5a83c73698c0c78056859aec0103b2482b0ed4d
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2024-03-22 14:36:07 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,8 +1,3 @@
 def solution(my_string, is_suffix):
-    suffix = []
-    
-    n = len(my_string)
-    for start in range(n):
-        suffix.append(my_string[start:n])
-        
-    return 1 if is_suffix in suffix else 0
+    answer = 0
+    return answer
