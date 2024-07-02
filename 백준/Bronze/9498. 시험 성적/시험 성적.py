b6cf864809bb0df9d6562b49de3af6d2748fb171
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2024-07-02 11:37:19 +0900

    [Bronze V] Title: 시험 성적, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+score = int(input())
+result = "F"
+
+if score >= 90:
+    result = "A"
+elif score >= 80:
+    result = "B"
+elif score >= 70:
+    result = "C"
+elif score >= 60:
+    result = "D"
+
+print(result)
