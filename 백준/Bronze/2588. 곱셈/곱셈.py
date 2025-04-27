a88947578a01fef21e3616306513c026eaa766e5
Hyunseok <109321543+HyunseokCheong@users.noreply.github.com>
2025-04-27 19:43:43 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,14 @@
+data = input()
+a = int(data[0])
+b = int(data[1])
+c = int(data[2])
+data = input()
+d = int(data[0])
+e = int(data[1])
+f = int(data[2])
+
+data = a * 100 + b * 10 + c
+print(f * data)
+print(e * data)
+print(d * data)
+print((d * 100 + e * 10 + f) * data)
